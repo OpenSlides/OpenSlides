c88ff41805365f8f729db17e99d24808a218bdf6
Emanuel Schuetze <emanuel@intevation.de>
2011-11-14 23:40:19 +0100

    Reordered config tab subpages.

--- openslides/system/views.py
+++ openslides/system/views.py
@@ -21,6 +21,100 @@ from system.forms import SystemConfigForm, EventConfigForm, AgendaConfigForm, Ap
 from system.api import config_get, config_set
 
 @permission_required('system.can_manage_system')
+@template('system/general.html')
+def get_general_config(request):
+    if request.method == 'POST':
+        form_event = EventConfigForm(request.POST, prefix='event')
+        if form_event.is_valid():
+            # event form
+            config_set('event_name', form_event.cleaned_data['event_name'])
+            config_set('event_description', form_event.cleaned_data['event_description'])
+            config_set('event_date', form_event.cleaned_data['event_date'])
+            config_set('event_location', form_event.cleaned_data['event_location'])
+            config_set('event_organizer', form_event.cleaned_data['event_organizer'])
+            messages.success(request, _('General settings successfully saved.'))
+        else:
+            messages.error(request, _('Please check the form for errors.'))
+    else:
+        form_event = EventConfigForm(initial={
+            'event_name': config_get('event_name'),
+        }, prefix='event')
+    return {
+        'form_event': form_event,
+    }
+
+@permission_required('system.can_manage_system')
+@template('system/agenda.html')
+def get_agenda_config(request):
+    if request.method == 'POST':
+        form_agenda = AgendaConfigForm(request.POST, prefix='agenda')
+        if form_agenda.is_valid():
+            config_set('agenda_countdown_time', form_agenda.cleaned_data['agenda_countdown_time'])
+            messages.success(request, _('Agenda settings successfully saved.'))
+        else:
+            messages.error(request, _('Please check the form for errors.'))
+    else:
+        form_agenda = AgendaConfigForm(initial={
+            'agenda_countdown_time': config_get('agenda_countdown_time'),
+        }, prefix='agenda')
+    return {
+        'form_agenda': form_agenda,
+    }
+
+@permission_required('system.can_manage_system')
+@template('system/application.html')
+def get_application_config(request):
+    if request.method == 'POST':
+        form_application = ApplicationConfigForm(request.POST, prefix='application')
+        form_assignment = AssignmentConfigForm(request.POST, prefix='assignment')
+        if form_application.is_valid():
+            config_set('application_min_supporters', form_application.cleaned_data['application_min_supporters'])
+            config_set('application_preamble', form_application.cleaned_data['application_preamble'])
+            config_set('application_pdf_ballot_papers_selection', form_application.cleaned_data['application_pdf_ballot_papers_selection'])
+            config_set('application_pdf_ballot_papers_number', form_application.cleaned_data['application_pdf_ballot_papers_number'])
+            config_set('application_pdf_title', form_application.cleaned_data['application_pdf_title'])
+            config_set('application_pdf_preamble', form_application.cleaned_data['application_pdf_preamble'])
+            messages.success(request, _('Application settings successfully saved.'))
+        else:
+            messages.error(request, _('Please check the form for errors.'))
+    else:
+        form_application = ApplicationConfigForm(initial={
+            'application_min_supporters': config_get('application_min_supporters'),
+            'application_preamble': config_get('application_preamble'),
+            'application_pdf_ballot_papers_selection': config_get('application_pdf_ballot_papers_selection'),
+            'application_pdf_ballot_papers_number': config_get('application_pdf_ballot_papers_number'),
+            'application_pdf_title': config_get('application_pdf_title'),
+            'application_pdf_preamble': config_get('application_pdf_preamble'),
+        }, prefix='application')
+    return {
+        'form_application': form_application,
+    }
+
+@permission_required('system.can_manage_system')
+@template('system/assignment.html')
+def get_assignment_config(request):
+    if request.method == 'POST':
+        form_assignment = AssignmentConfigForm(request.POST, prefix='assignment')
+        if form_assignment.is_valid():
+            config_set('assignment_pdf_ballot_papers_selection', form_assignment.cleaned_data['assignment_pdf_ballot_papers_selection'])
+            config_set('assignment_pdf_ballot_papers_number', form_assignment.cleaned_data['assignment_pdf_ballot_papers_number'])
+            config_set('assignment_pdf_title', form_assignment.cleaned_data['assignment_pdf_title'])
+            config_set('assignment_pdf_preamble', form_assignment.cleaned_data['assignment_pdf_preamble'])
+            messages.success(request, _('Election settings successfully saved.'))
+        else:
+            messages.error(request, _('Please check the form for errors.'))
+    else:
+        form_assignment = AssignmentConfigForm(initial={
+            'assignment_pdf_ballot_papers_selection': config_get('assignment_pdf_ballot_papers_selection'),
+            'assignment_pdf_ballot_papers_number': config_get('assignment_pdf_ballot_papers_number'),
+            'assignment_pdf_title': config_get('assignment_pdf_title'),
+            'assignment_pdf_preamble': config_get('assignment_pdf_preamble'),
+        }, prefix='assignment')
+    return {
+        'form_assignment': form_assignment,
+    }
+
+@permission_required('system.can_manage_system')
 @template('system/system.html')
 def get_system_config(request):
     if request.method == 'POST':
@@ -47,7 +141,6 @@ def get_system_config(request):
             messages.success(request, _('System settings successfully saved.'))
         else:
             messages.error(request, _('Please check the form for errors.'))
-
     else:
         form = SystemConfigForm(initial={
             'system_url': config_get('system_url'),
@@ -56,69 +149,4 @@ def get_system_config(request):
         })
     return {
         'form': form,
-    }
-
-@permission_required('system.can_manage_system')
-@template('system/general.html')
-def get_general_config(request):
-    if request.method == 'POST':
-        form_event = EventConfigForm(request.POST, prefix='event')
-        form_agenda = AgendaConfigForm(request.POST, prefix='agenda')
-        form_application = ApplicationConfigForm(request.POST, prefix='application')
-        form_assignment = AssignmentConfigForm(request.POST, prefix='assignment')
-        if form_event.is_valid() and form_agenda.is_valid() and form_application.is_valid() and form_assignment.is_valid():
-            # event form
-            config_set('event_name', form_event.cleaned_data['event_name'])
-            config_set('event_description', form_event.cleaned_data['event_description'])
-            config_set('event_date', form_event.cleaned_data['event_date'])
-            config_set('event_location', form_event.cleaned_data['event_location'])
-            config_set('event_organizer', form_event.cleaned_data['event_organizer'])
-            # agenda form
-            config_set('agenda_countdown_time', form_agenda.cleaned_data['agenda_countdown_time'])
-            # application form
-            config_set('application_min_supporters', form_application.cleaned_data['application_min_supporters'])
-            config_set('application_preamble', form_application.cleaned_data['application_preamble'])
-            config_set('application_pdf_ballot_papers_selection', form_application.cleaned_data['application_pdf_ballot_papers_selection'])
-            config_set('application_pdf_ballot_papers_number', form_application.cleaned_data['application_pdf_ballot_papers_number'])
-            config_set('application_pdf_title', form_application.cleaned_data['application_pdf_title'])
-            config_set('application_pdf_preamble', form_application.cleaned_data['application_pdf_preamble'])
-            # assignment form
-            config_set('assignment_pdf_ballot_papers_selection', form_assignment.cleaned_data['assignment_pdf_ballot_papers_selection'])
-            config_set('assignment_pdf_ballot_papers_number', form_assignment.cleaned_data['assignment_pdf_ballot_papers_number'])
-            config_set('assignment_pdf_title', form_assignment.cleaned_data['assignment_pdf_title'])
-            config_set('assignment_pdf_preamble', form_assignment.cleaned_data['assignment_pdf_preamble'])
-            messages.success(request, _('General settings successfully saved.'))
-        else:
-            messages.error(request, _('Please check the form for errors.'))
-
-    else:
-        form_event = EventConfigForm(initial={
-            'event_name': config_get('event_name'),
-            'event_description': config_get('event_description'),
-            'event_date': config_get('event_date'),
-            'event_location': config_get('event_location'),
-            'event_organizer': config_get('event_organizer'),
-        }, prefix='event')
-        form_agenda = AgendaConfigForm(initial={
-            'agenda_countdown_time': config_get('agenda_countdown_time'),
-        }, prefix='agenda')
-        form_application = ApplicationConfigForm(initial={
-            'application_min_supporters': config_get('application_min_supporters'),
-            'application_preamble': config_get('application_preamble'),
-            'application_pdf_ballot_papers_selection': config_get('application_pdf_ballot_papers_selection'),
-            'application_pdf_ballot_papers_number': config_get('application_pdf_ballot_papers_number'),
-            'application_pdf_title': config_get('application_pdf_title'),
-            'application_pdf_preamble': config_get('application_pdf_preamble'),
-        }, prefix='application')
-        form_assignment = AssignmentConfigForm(initial={
-            'assignment_pdf_ballot_papers_selection': config_get('assignment_pdf_ballot_papers_selection'),
-            'assignment_pdf_ballot_papers_number': config_get('assignment_pdf_ballot_papers_number'),
-            'assignment_pdf_title': config_get('assignment_pdf_title'),
-            'assignment_pdf_preamble': config_get('assignment_pdf_preamble'),
-        }, prefix='assignment')
-    return {
-        'form_event': form_event,
-        'form_agenda': form_agenda,
-        'form_application': form_application,
-        'form_assignment': form_assignment,
     }
