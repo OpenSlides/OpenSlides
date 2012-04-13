a1c5766e7a805af501c734476d6e4c833a020752
Emanuel Schuetze <emanuel@intevation.de>
2012-04-13 21:43:16 +0200

    Adjusted app prefix in urls.py (outsource in global urls.py). Some code formatting.

--- openslides/application/urls.py
+++ openslides/application/urls.py
@@ -15,82 +15,125 @@ from django.utils.translation import ugettext as _
 
 from application.views import ViewPoll, ActivatePoll
 
-
 urlpatterns = patterns('application.views',
-    url(r'^application/$', 'overview', \
-        name='application_overview'),
+    url(r'^$',
+        'overview',
+        name='application_overview',
+    ),
 
-    url(r'^application/(?P<application_id>\d+)/$', 'view', \
-        name='application_view'),
+    url(r'^(?P<application_id>\d+)/$',
+        'view',
+        name='application_view',
+    ),
 
-    url(r'^application/(?P<application_id>\d+)/newest/$', 'view', {'newest': True}, \
-        name='application_view_newest'),
+    url(r'^(?P<application_id>\d+)/newest/$',
+        'view',
+        {'newest': True},
+        name='application_view_newest',
+    ),
 
-    url(r'^application/new/$', 'edit', \
-        name='application_new'),
+    url(r'^new/$',
+        'edit',
+        name='application_new',
+    ),
 
-    url(r'^application/import/$', 'application_import', \
-        name='application_import'),
+    url(r'^import/$',
+        'application_import',
+        name='application_import',
+    ),
 
-    url(r'^application/(?P<application_id>\d+)/edit/$', 'edit', \
-        name='application_edit'),
+    url(r'^(?P<application_id>\d+)/edit/$',
+        'edit',
+        name='application_edit',
+    ),
 
-    url(r'^application/(?P<application_id>\d+)/del/$', 'delete', \
-        name='application_delete'),
+    url(r'^(?P<application_id>\d+)/del/$',
+        'delete',
+        name='application_delete',
+    ),
 
-    url(r'^application/(?P<application_id>\d+)/setnumber/$', 'set_number', \
-        name='application_set_number'),
+    url(r'^(?P<application_id>\d+)/setnumber/$',
+        'set_number',
+        name='application_set_number',
+    ),
 
-    url(r'^application/(?P<application_id>\d+)/setstatus/' \
-        '(?P<status>[a-z]{3})/$', 'set_status', \
-        name='application_set_status'),
+    url(r'^(?P<application_id>\d+)/setstatus/' '(?P<status>[a-z]{3})/$',
+        'set_status',
+        name='application_set_status',
+    ),
 
-    url(r'^application/(?P<application_id>\d+)/permit/$', 'permit', \
-        name='application_permit'),
+    url(r'^(?P<application_id>\d+)/permit/$',
+        'permit',
+        name='application_permit',
+    ),
 
-    url(r'^application/version/(?P<aversion_id>\d+)/permit/$', 'permit_version', \
-        name='application_version_permit'),
+    url(r'^version/(?P<aversion_id>\d+)/permit/$',
+        'permit_version',
+        name='application_version_permit',
+    ),
 
-    url(r'^application/version/(?P<aversion_id>\d+)/reject/$', 'reject_version', \
-        name='application_version_reject'),
+    url(r'^version/(?P<aversion_id>\d+)/reject/$',
+        'reject_version',
+        name='application_version_reject',
+    ),
 
-    url(r'^application/(?P<application_id>\d+)/notpermit/$', 'notpermit', \
-        name='application_notpermit'),
+    url(r'^(?P<application_id>\d+)/notpermit/$',
+        'notpermit',
+        name='application_notpermit',
+    ),
 
-    url(r'^application/(?P<application_id>\d+)/reset/$', 'reset', \
-        name='application_reset'),
+    url(r'^(?P<application_id>\d+)/reset/$',
+        'reset',
+        name='application_reset',
+    ),
 
-    url(r'^application/(?P<application_id>\d+)/support/$', 'support', \
-        name='application_support'),
+    url(r'^(?P<application_id>\d+)/support/$',
+        'support',
+        name='application_support',
+    ),
 
-    url(r'^application/(?P<application_id>\d+)/unsupport/$', 'unsupport', \
-        name='application_unsupport'),
+    url(r'^(?P<application_id>\d+)/unsupport/$',
+        'unsupport',
+        name='application_unsupport',
+    ),
 
-    url(r'^application/(?P<application_id>\d+)/set_active/$', 'set_active',
-        name='application_activate_item'),
+    url(r'^(?P<application_id>\d+)/set_active/$',
+        'set_active',
+        name='application_activate_item',
+    ),
 
-    url(r'^application/(?P<application_id>\d+)/gen_poll/$', 'gen_poll', \
-        name='application_gen_poll'),
+    url(r'^(?P<application_id>\d+)/gen_poll/$',
+        'gen_poll',
+        name='application_gen_poll',
+    ),
 
-    url(r'^application/print/$', 'print_application', \
-        name='print_applications'),
+    url(r'^print/$',
+        'print_application',
+        name='print_applications',
+    ),
 
-    url(r'^application/(?P<application_id>\d+)/print/$', 'print_application', \
-        name='print_application'),
+    url(r'^(?P<application_id>\d+)/print/$',
+        'print_application',
+        name='print_application',
+    ),
 
-    url(r'^application/poll/(?P<poll_id>\d+)/print/$', 'print_application_poll', \
-        name='print_application_poll'),
+    url(r'^poll/(?P<poll_id>\d+)/print/$',
+        'print_application_poll',
+        name='print_application_poll',
+    ),
 
-    url(r'^application/poll/(?P<poll_id>\d+)/$',
+    url(r'^poll/(?P<poll_id>\d+)/$',
         ViewPoll.as_view(),
         name='application_poll_view',
     ),
 
-    url(r'^application/poll/(?P<poll_id>\d+)/activate/$',
+    url(r'^poll/(?P<poll_id>\d+)/activate/$',
         ActivatePoll.as_view(),
         name='application_poll_activate',
     ),
 
-    url(r'^application/poll/(?P<poll_id>\d+)/del/$', 'delete_poll', \
-        name='application_poll_delete'),
+    url(r'^poll/(?P<poll_id>\d+)/del/$',
+        'delete_poll',
+        name='application_poll_delete',
+    ),
 )
--- openslides/assignment/urls.py
+++ openslides/assignment/urls.py
@@ -15,65 +15,106 @@ from django.conf.urls.defaults import *
 from assignment.views import ViewPoll #, ActivatePoll
 
 urlpatterns = patterns('assignment.views',
-    url(r'^assignment/$', 'get_overview', \
-        name='assignment_overview'),
-
-    url(r'^assignment/(?P<assignment_id>\d+)$', 'view', \
-        name='assignment_view'),
-
-    url(r'^assignment/new$', 'edit', \
-        name='assignment_new'),
-
-    url(r'^assignment/(?P<assignment_id>\d+)/edit$', 'edit', \
-        name='assignment_edit'),
-
-    url(r'^assignment/(?P<assignment_id>\d+)/del$', 'delete', \
-        name='assignment_delete'),
-
-    url(r'^assignment/(?P<assignment_id>\d+)/setstatus/(?P<status>[a-z]{3})$', 'set_status', \
-        name='assignment_set_status'),
-
-    url(r'^assignment/(?P<assignment_id>\d+)/run$', 'run', \
-        name='assignment_run'),
-
-    url(r'^assignment/(?P<assignment_id>\d+)/delrun$', 'delrun', \
-        name='assignment_delrun'),
-
-    url(r'^assignment/(?P<assignment_id>\d+)/delother/(?P<profile_id>\d+)$', 'delother', \
-        name='assignment_delother'),
-
-    url(r'^assignment/(?P<assignment_id>\d+)/set_active/$', 'set_active',
-        name='assignment_activate_item'),
-
-    url(r'^assignment/poll/(?P<poll_id>\d+)/print$', 'print_assignment_poll', \
-        name='print_assignment_poll'),
-
-    url(r'^assignment/print$', 'print_assignment', \
-        name='print_assignment'),
-
-    url(r'^assignment/(?P<assignment_id>\d+)/print$', 'print_assignment', \
-        name='print_assignment'),
-
-    url(r'^assignment/(?P<assignment_id>\d+)/gen_poll$', 'gen_poll', \
-        name='assignment_gen_poll'),
-
-    url(r'^assignment/poll/(?P<poll_id>\d+)$',
-        ViewPoll.as_view(),
-        name='assignment_poll_view'
+    url(r'^$',
+        'get_overview',
+        name='assignment_overview',
     ),
 
-    url(r'^assignment/poll/(?P<poll_id>\d+)/del$', 'delete_poll', \
-        name='assignment_poll_delete'),
+    url(r'^(?P<assignment_id>\d+)$',
+        'view',
+        name='assignment_view'),
 
-    url(r'^assignment/poll/(?P<poll_id>\d+)/pub/$', 'set_published', {'published': True},
-        name='assignment_poll_publish'),
+    url(r'^new$',
+        'edit',
+        name='assignment_new',
+    ),
 
-    url(r'^assignment/poll/(?P<poll_id>\d+)/notpub/$', 'set_published', {'published': False},
-        name='assignment_poll_notpublish'),
+    url(r'^(?P<assignment_id>\d+)/edit$',
+        'edit',
+        name='assignment_edit',
+    ),
 
-    url(r'^assignment/(?P<assignment_id>\d+)/elected/(?P<profile_id>\d+)$', 'set_elected', {'elected': True}, \
-        name='assignment_user_elected'),
+    url(r'^(?P<assignment_id>\d+)/del$',
+        'delete',
+        name='assignment_delete',
+    ),
 
-    url(r'^assignment/(?P<assignment_id>\d+)/notelected/(?P<profile_id>\d+)$', 'set_elected', {'elected': False}, \
-        name='assignment_user_not_elected'),
+    url(r'^(?P<assignment_id>\d+)/setstatus/(?P<status>[a-z]{3})$',
+        'set_status',
+        name='assignment_set_status',
+    ),
+
+    url(r'^(?P<assignment_id>\d+)/run$',
+        'run',
+        name='assignment_run',
+    ),
+
+    url(r'^(?P<assignment_id>\d+)/delrun$',
+        'delrun',
+        name='assignment_delrun',
+    ),
+
+    url(r'^(?P<assignment_id>\d+)/delother/(?P<profile_id>\d+)$',
+        'delother',
+        name='assignment_delother',
+    ),
+
+    url(r'^(?P<assignment_id>\d+)/set_active/$',
+        'set_active',
+        name='assignment_activate_item',
+    ),
+
+    url(r'^poll/(?P<poll_id>\d+)/print$',
+        'print_assignment_poll',
+        name='print_assignment_poll',
+    ),
+
+    url(r'^print$',
+        'print_assignment',
+        name='print_assignment',
+    ),
+
+    url(r'^(?P<assignment_id>\d+)/print$',
+        'print_assignment',
+        name='print_assignment',
+    ),
+
+    url(r'^(?P<assignment_id>\d+)/gen_poll$',
+        'gen_poll',
+        name='assignment_gen_poll',
+    ),
+
+    url(r'^poll/(?P<poll_id>\d+)$',
+        ViewPoll.as_view(),
+        name='assignment_poll_view',
+    ),
+
+    url(r'^poll/(?P<poll_id>\d+)/del$',
+        'delete_poll',
+        name='assignment_poll_delete',
+    ),
+
+    url(r'^poll/(?P<poll_id>\d+)/pub/$',
+        'set_published',
+        {'published': True},
+        name='assignment_poll_publish',
+    ),
+
+    url(r'^poll/(?P<poll_id>\d+)/notpub/$',
+        'set_published',
+        {'published': False},
+        name='assignment_poll_notpublish',
+    ),
+
+    url(r'^(?P<assignment_id>\d+)/elected/(?P<profile_id>\d+)$',
+        'set_elected',
+        {'elected': True},
+        name='assignment_user_elected',
+    ),
+
+    url(r'^(?P<assignment_id>\d+)/notelected/(?P<profile_id>\d+)$',
+        'set_elected',
+        {'elected': False},
+        name='assignment_user_not_elected',
+    ),
 )
