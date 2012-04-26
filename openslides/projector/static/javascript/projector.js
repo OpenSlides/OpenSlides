aac48a0b0250516bacb38f9f4c8734ef030ed16e
Emanuel Schuetze <emanuel@intevation.de>
2012-04-26 08:52:20 +0200

    Redesign overlays.

--- openslides/projector/static/javascript/projector.js
+++ openslides/projector/static/javascript/projector.js
@@ -27,7 +27,10 @@ function presentation_reload() {
                 $('#scrollcontent').animate({'margin-top': data.up + 'em'}, 200);
                 // overlays
                 $('#overlays div').remove();
+                $('#overlays span').remove();
                 $.each(data['overlays'], function (index, value){
+                    if (value[0] != "Countdown")
+                        $('#overlays').append('<span id="overlay_transparent"></span>')
                     $('#overlays').append('<div id="overlay_' + value[0] + '">' + value[1] + '</div>');
                 });
                 setTimeout("presentation_reload()", 500);
