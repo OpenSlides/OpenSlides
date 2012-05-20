760bb415ae6fa9e9f4145f0921d3efdf2185be16
Emanuel Schuetze <emanuel@intevation.de>
2012-05-20 21:56:11 +0200

    Added missing license header.

--- extras/benchmark/bench.py
+++ extras/benchmark/bench.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Benchmark test script for OpenSlides.
+
+    :copyright: 2012 by OpenSlides team, see AUTHORS.
+    :license: GNU GPL, see LICENSE for more details.
+"""
+
 import argparse
 import multiprocessing
 import random
