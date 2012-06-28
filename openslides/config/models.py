587ce4093d49a03d358e78480fa4039eed5ab188
Oskar Hahn <mail@oshahn.de>
2012-06-29 00:27:27 +0200

    update translation

--- openslides/config/models.py
+++ openslides/config/models.py
@@ -99,7 +99,7 @@ def default_config(sender, key, **kwargs):
         'frontpage_title': _('Welcome'),
         'frontpage_welcometext': _('Welcome to OpenSlides!'),
         'show_help_text': True,
-        'help_text': _("Get professional support for OpenSlides on <a href='http://openslides.org/' target='_blank'>www.openslides.org</a>."),
+        'help_text': "%s <a href='http://openslides.org/' target='_blank'>www.openslides.org</a>." % _("Get professional support for OpenSlides on"),
         'system_enable_anonymous': False,
     }.get(key)
 
