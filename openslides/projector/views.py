a149d5a6bc57e28d3755e7e8e665447b60bd8481
Norman Jäckel <hg@normanjaeckel.de>
2012-07-04 03:19:42 +0200

    Ticket #229

--- openslides/projector/views.py
+++ openslides/projector/views.py
@@ -329,6 +329,7 @@ def get_widgets(request):
             template='projector/widget.html',
             context={
                 'slides': ProjectorSlide.objects.all(),
+                'welcomepage_is_active': not bool(config["presentation"]),
             }
         ),
     ]
