07c46492439b6518101180f3828aac9b50d66439
Oskar Hahn <mail@oshahn.de>
2012-07-16 14:05:51 +0200

    #297 fix error in ApplicationPDF with changes in the poll API

--- openslides/application/models.py
+++ openslides/application/models.py
@@ -478,7 +478,9 @@ class Application(models.Model, SlideMixin):
         for poll in self.polls:
             for option in poll.get_options():
                 if option.get_votes().exists():
-                    results.append((option.Yes, option.No, option.Abstain, poll.print_votesinvalid(), poll.print_votescast()))
+                    results.append((option['Yes'], option['No'],
+                        option['Abstain'], poll.print_votesinvalid(),
+                        poll.print_votescast()))
         return results
 
 
