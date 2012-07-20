8854f7f9ba6b79e6ded655d9225babd10215aa5c
Oskar Hahn <mail@oshahn.de>
2012-07-20 11:34:37 +0200

    #319 sort participants by there last_name by default

--- openslides/participant/views.py
+++ openslides/participant/views.py
@@ -94,7 +94,7 @@ def get_overview(request):
         elif sortfilter['sort'][0] in ['group', 'type', 'committee', 'comment']:
             query = query.order_by('profile__%s' % sortfilter['sort'][0])
     else:
-        query = query.order_by('first_name')
+        query = query.order_by('last_name')
     if 'reverse' in sortfilter:
         query = query.reverse()
 
