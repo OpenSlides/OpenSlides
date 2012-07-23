769bceb80e8becf8ccd8a0f390873ca75df3e18f
Emanuel Schuetze <emanuel@intevation.de>
2012-07-23 22:17:24 +0200

    Website: Added order form. Some minor changes.

--- extras/website/website/forms.py
+++ extras/website/website/forms.py
@@ -1,17 +1,29 @@
 from django import forms
-
-TOPIC_CHOICES = (
-    ('question', 'General question'),
-    ('bug', 'Bug report'),
-    ('reference', 'Add OpenSlides reference'),
-)
+from django.utils.translation import ugettext as _
 
 class ContactForm(forms.Form):
     error_css_class = 'error'
     required_css_class = 'required'
-    topic = forms.ChoiceField(choices=TOPIC_CHOICES)
-    subject = forms.CharField(max_length=100)
-    message = forms.CharField()
-    sender = forms.EmailField()
-    cc_myself = forms.BooleanField(required=False)
+    subject = forms.CharField(label=_("Betreff"))
+    message = forms.CharField(widget=forms.Textarea(), label=_("Nachricht"))
+    sender = forms.EmailField(label=_("Absender"))
+    cc_myself = forms.BooleanField(required=False,label=_("Kopie an meine Adresse"))
 
+class OrderEventForm(forms.Form):
+    error_css_class = 'error'
+    required_css_class = 'required'
+    event_name = forms.CharField(label=_("Veranstaltungsname"))
+    event_description = forms.CharField(max_length=100, label=_("Kurzbeschreibung der Veranstaltung"))
+    event_date = forms.CharField(label=_("Veranstaltungszeitraum"))
+    event_location = forms.CharField(label=_("Veranstaltungsort"))
+
+class OrderContactForm(forms.Form):
+    error_css_class = 'error'
+    required_css_class = 'required'
+    contact_organisation = forms.CharField(label=_("Organisation"))
+    contact_street = forms.CharField(label=_("Strasse"))
+    contact_postcode = forms.CharField(label=_("PLZ"))
+    contact_location = forms.CharField(label=_("Ort"))
+    contact_name = forms.CharField(label=_("Ansprechpartner"))
+    contact_phone = forms.CharField(label=_("Telefon"))
+    contact_email = forms.EmailField(label=_("E-Mail"))
--- extras/website/website/views.py
+++ extras/website/website/views.py
@@ -2,19 +2,12 @@
 from django.shortcuts import render_to_response, render
 from django.http import HttpResponseRedirect
 
-from website.forms import ContactForm
-
-#class TemplateView(TemplateView):
-#    def get_context_data(self, **kwargs):
-#        context = super(TemplateView, self).get_context_data(**kwargs)
-#        context[request] = self.request
-#        return context
-
+from website.forms import ContactForm, OrderEventForm, OrderContactForm
 
 def contactform(request):
-    if request.method == 'POST': # If the form has been submitted...
-        form = ContactForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
             subject = form.cleaned_data['subject']
             message = form.cleaned_data['message']
             sender = form.cleaned_data['sender']
@@ -22,13 +15,42 @@ def contactform(request):
             recipients = ['emanuel@intevation.de']
             if cc_myself:
                 recipients.append(sender)
-
             from django.core.mail import send_mail
             send_mail(subject, message, sender, recipients)
-            return HttpResponseRedirect('/demo/') # Redirect after POST
+            return HttpResponseRedirect('/contact/')
     else:
-        form = ContactForm() # An unbound form
-
+        form = ContactForm()
     return render(request, 'contact-form.html', {
         'form': form,
+    })
+
+
+def orderform(request, package='2'):
+    if request.method == 'POST':
+        form_event = OrderEventForm(request.POST)
+        form_contact = OrderContactForm(request.POST)
+        if form_event.is_valid() and form_contact.is_valid():
+            # event
+            event_name = form_event.cleaned_data['event_name']
+            event_description = form_event.cleaned_data['event_description']
+            event_date = form_event.cleaned_data['event_date']
+            event_location = form_event.cleaned_data['event_location']
+            # contact
+            contact_name = form_contact.cleaned_data['contact_name']
+            contact_phone = form_contact.cleaned_data['contact_phone']
+            contact_email = form_contact.cleaned_data['contact_email']
+            # mail
+            recipients = ['emanuel@intevation.de']
+#            recipients.append(contact_email)
+            message = "%s" % event_name
+            from django.core.mail import send_mail
+            send_mail("Bestellung", message, contact_email, recipients)
+            return HttpResponseRedirect('/pricing/thanks')
+    else:
+        form_event = OrderEventForm()
+        form_contact = OrderContactForm()
+    return render(request, 'order-form.html', {
+        'form_event': form_event,
+        'form_contact': form_contact,
+        'package': package,
     })
