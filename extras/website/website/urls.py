f5a400a9aefc8ced6f2139888b1d784a78cb3074
Oskar Hahn <mail@oshahn.de>
2012-07-31 10:25:57 +0200

    Website: Include authors

--- extras/website/website/urls.py
+++ extras/website/website/urls.py
@@ -29,6 +29,7 @@ urlpatterns = i18n_patterns('',
     url(r'^about/press/20110915/$', TemplateView.as_view(template_name="pm-20110915.html"), name='pm-20110915',),
     url(r'^about/press/20120416/$', TemplateView.as_view(template_name="pm-20120416.html"), name='pm-20120416',),
     url(r'^about/press/20120726/$', TemplateView.as_view(template_name="pm-20120726.html"), name='pm-20120726',),
+    url(r'^authors/$', TemplateView.as_view(template_name="authors.html"), name='authors',),
 )
 
 urlpatterns += patterns('django.contrib.staticfiles.views',
