b3a86ec44fd46ebd1d3edac59ac50cbb93d3b95f
Stefam Frauenknecht <stefan.frauenknecht@gmail.com>
2012-08-30 16:49:00 +0200

    Revert changes

--- openslides/agenda/forms.py
+++ openslides/agenda/forms.py
@@ -12,6 +12,7 @@
 
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext_lazy as _, ugettext_noop
 
 from mptt.forms import TreeNodeChoiceField
 
