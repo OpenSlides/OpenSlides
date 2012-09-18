154847906902021c06c74115c6ac76859ce24ea1
Emanuel Schuetze <emanuel@intevation.de>
2012-09-18 22:47:12 +0200

    Fixed application and assigment ballot paper: don't use old profile object.

--- openslides/assignment/views.py
+++ openslides/assignment/views.py
@@ -554,9 +554,9 @@ class AssignmentPollPDF(PDFView):
 
         # set number of ballot papers
         if ballot_papers_selection == "NUMBER_OF_DELEGATES":
-            number = User.objects.filter(profile__type__iexact="delegate").count()
+            number = User.objects.filter(type__iexact="delegate").count()
         elif ballot_papers_selection == "NUMBER_OF_ALL_PARTICIPANTS":
-            number = int(Profile.objects.count())
+            number = int(User.objects.count())
         else: # ballot_papers_selection == "CUSTOM_NUMBER"
             number = int(ballot_papers_number)
         number = max(1, number)
