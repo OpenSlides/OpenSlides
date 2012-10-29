b213e75223385a4a1037c65eeddeec6a25566099
Norman Jäckel <my-git-mail@normanjaeckel.de>
2012-10-29 10:16:12 +0100

    Enhancement to ticket # 389 (Message on assignment slide when no vote results yet available)

--- openslides/assignment/models.py
+++ openslides/assignment/models.py
@@ -227,11 +227,12 @@ class Assignment(models.Model, SlideMixin):
         """
         return the slide dict
         """
+        polls = self.poll_set
         data = super(Assignment, self).slide()
         data['assignment'] = self
         data['title'] = self.name
-        data['polls_available'] = self.poll_set.exists()
-        data['polls'] = self.poll_set.filter(published=True)
+        data['some_polls_available'] = polls.exists()
+        data['polls'] = polls.filter(published=True)
         data['vote_results'] = self.vote_results(only_published=True)
         data['assignment_publish_winner_results_only'] = \
             config['assignment_publish_winner_results_only']
