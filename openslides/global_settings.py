4c9729159835bb796a30ff39042c5bd8c69ae6d5
Norman Jäckel <my-git-mail@normanjaeckel.de>
2012-11-23 11:33:34 +0100

    Remove non-used revision template var.

--- openslides/global_settings.py
+++ openslides/global_settings.py
@@ -132,7 +132,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.request',
     'django.core.context_processors.i18n',
     'django.core.context_processors.static',
-    'openslides.utils.utils.revision',
     'openslides.utils.auth.anonymous_context_additions',
 )
 
