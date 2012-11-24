fddf929ab5e7350407d1b245d452c0b203369a55
Oskar Hahn <mail@oshahn.de>
2012-11-25 00:58:19 +0100

    Load groups_de when creating the database

--- openslides/main.py
+++ openslides/main.py
@@ -252,6 +252,7 @@ def run_syncdb():
     # now initialize the database
     argv = ["", "syncdb", "--noinput"]
     execute_from_command_line(argv)
+    execute_from_command_line(["", "loaddata", "groups_de"])
 
 
 def set_system_url(url):
