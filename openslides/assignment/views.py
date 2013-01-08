5dd42b85a5b07ed931399c4e19863cc7893da53c
Oskar Hahn <mail@oshahn.de>
2013-01-08 21:42:25 +0100

    fix pep8 errors

--- openslides/assignment/views.py
+++ openslides/assignment/views.py
@@ -318,7 +318,7 @@ class AssignmentPollDelete(DeleteView):
         return reverse('assignment_view', args=[self.assignment.id])
 
     def get_success_message(self):
-        return  _('Ballot was successfully deleted.') % self.object
+        return _('Ballot was successfully deleted.') % self.object
 
 
 class AssignmentPDF(PDFView):
