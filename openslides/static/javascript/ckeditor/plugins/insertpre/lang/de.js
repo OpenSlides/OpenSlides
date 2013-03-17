41146f3bebc9c9e4e0795df030f78fc84905abb9
Tobias Hößl <tobias@hoessl.eu>
2013-03-17 17:16:37 +0100

    Initial attempt to integrate CKEditor into OpenSlides

---
+++ openslides/static/javascript/ckeditor/plugins/insertpre/lang/de.js
@@ -0,0 +1,6 @@
+CKEDITOR.plugins.setLang( 'insertpre', 'de', {
+	title : 'Blocksatz-Text eingeben',
+	code : 'Blocksatz',
+	edit : 'Text bearbeiten',
+	notEmpty : 'Das Textfeld darf nicht leer bleiben.'
+});
