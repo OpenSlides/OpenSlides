5d472b274ac39aa07d5edd3a39c0ecdebcc07d84
Norman Jäckel <my-git-mail@normanjaeckel.de>
2013-04-03 17:55:30 +0200

    Update tests to extend test cover by coverage

---
+++ tests/person_api/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    openslides.person_api
+    ~~~~~~~~~~~~~~~~~~~~~
+
+    Unit test for the person api.
+
+    :copyright: 2011–2013 by OpenSlides team, see AUTHORS.
+    :license: GNU GPL, see LICENSE for more details.
+"""
+
+VERSION = (9999, 9999, 9999, 'alpha', 1)
