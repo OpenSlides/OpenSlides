4f68dab628f0c6ef22297cbf0f651d3e36162275
Emanuel Schuetze <emanuel@intevation.de>
2013-05-07 23:36:32 +0200

    Move 'status_link' function from participants.js to utils.js for using in VoteCollector plugin. Added hook 'post_js' for VoteCollector plugin. Fixed poll id in poll_form.

--- openslides/participant/static/javascript/participant.js
+++ openslides/participant/static/javascript/participant.js
@@ -1,30 +1,7 @@
 /**
  * OpenSlides participants functions
  *
- * :copyright: 2011, 2012 by OpenSlides team, see AUTHORS.
+ * :copyright: 2013 by OpenSlides team, see AUTHORS.
  * :license: GNU GPL, see LICENSE for more details.
  */
 
-$(function() {
-    // change participant status (on/off)
-    $('.status_link').click(function(event) {
-        event.preventDefault();
-        var link = $(this);
-        $.ajax({
-            type: 'GET',
-            url: $(this).attr('href'),
-            dataType: 'json',
-            success: function(data) {
-                if (data.active) {
-                    newclass = 'icon-on';
-                    link.addClass('btn-success');
-                } else {
-                    newclass = 'icon-off';
-                    link.removeClass('btn-success');
-                }
-                link.children('i').removeClass('icon-off icon-on').addClass(newclass);
-                link.attr('href', data.link);
-            }
-        });
-    });
-});
