69c783270189b493ce5e2833034499d146082a29
Emanuel Schuetze <emanuel@intevation.de>
2013-07-08 10:11:48 +0200

    Updated csv example files for motion and participants - in DE and EN used in demo database. Updated motion csv import test.

--- tests/motion/test_csv_import.py
+++ tests/motion/test_csv_import.py
@@ -33,32 +33,28 @@ class CSVImport(TestCase):
         self.normal_client.login(username='User_CiuNgo4giqueeChie5oi', password='eihi1Eequaek4eagaiKu')
 
         # Category
-        self.category1 = Category.objects.create(name='Satzung', prefix='S')
-        self.category2 = Category.objects.create(name='Resolution', prefix='R1')
-        self.category3 = Category.objects.create(name='Resolution', prefix='R2')
+        self.category1 = Category.objects.create(name='Satzungsanträge', prefix='SA')
+        self.category2 = Category.objects.create(name='Bildung', prefix='B1')
+        self.category3 = Category.objects.create(name='Bildung', prefix='B2')
 
     def test_example_file_de(self):
-        special_user = User.objects.create_user(username='Fritz_Fleiner',
-                                                email='fritz@fritz.fritz',
+        special_user = User.objects.create_user(username='Harry_Holland',
                                                 password='iegheeChaje7guthie4a',
-                                                first_name='Fritz',
-                                                last_name='Fleiner')
-
+                                                first_name='Harry',
+                                                last_name='Holland')
         for i in range(2):
-            username = 'Hilde_Müller_%d' % i
+            username = 'John_Doe_%d' % i
             User.objects.create_user(username=username,
-                                     email='hilde@hilde.hilde',
                                      password='default',
-                                     first_name='Hilde',
-                                     last_name='Müller',
-                                     title='Dr.')
+                                     first_name='John',
+                                     last_name='Doe')
 
         csv_dir = os.path.join(os.path.dirname(__file__), '..', '..', 'extras', 'csv-examples')
         self.assertEqual(Motion.objects.count(), 0)
         with open(csv_dir + '/motions-demo_de.csv') as f:
             count_success, count_lines, error_messages, warning_messages = import_motions(csv_file=f, default_submitter=self.normal_user.person_id)
-        self.assertEqual(Motion.objects.count(), 4)
-        self.assertEqual(count_success, 4)
+        self.assertEqual(Motion.objects.count(), 11)
+        self.assertEqual(count_success, 11)
 
         motion1 = Motion.objects.get(pk=1)
         self.assertEqual(motion1.identifier, '1')
@@ -72,16 +68,18 @@ class CSVImport(TestCase):
         self.assertTrue(any('Category unknown.' in w for w in warning_messages))
 
         motion2 = Motion.objects.get(pk=2)
-        self.assertEqual(motion2.identifier, 'S 2')
+        self.assertEqual(motion2.identifier, 'SA 1')
         self.assertEqual(motion2.title, u'Satzungsänderung § 2 Abs. 3')
-        self.assertEqual(motion2.text, u'Die Versammlung möge beschließen, die Satzung in § 2 Abs. 3 wie folgt zu ändern:\n\n'
-                                       u'Es wird nach dem Wort "Zweck" der Satz "..." eingefügt.')
+        self.assertHTMLEqual(motion2.text, u'''<p>Die Versammlung möge beschließen, die Satzung in § 2 Abs. 3 wie folgt zu ändern:</p>
+                                               <p>Es wird vor dem Wort "Zweck" das Wort "gemeinnütziger" eingefügt.</p>''')
         self.assertEqual(motion2.reason, u'Die Änderung der Satzung ist aufgrund der letzten Erfahrungen eine sinnvolle Maßnahme, weil ...')
         self.assertEqual(len(motion2.submitter.all()), 1)
         self.assertEqual(motion2.submitter.all()[0].person, special_user)
         self.assertEqual(motion2.category, self.category1)
 
+        # check user 'John Doe'
         self.assertTrue(any('Several suitable submitters found.' in w for w in warning_messages))
+        # check category 'Bildung'
         self.assertTrue(any('Several suitable categories found.' in w for w in warning_messages))
 
     def test_malformed_file(self):
