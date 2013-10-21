ba2109cbfe981f9f15cc73f8c24e72b55b369fe3
Dominik Breu <dominikbreu@yahoo.de>
2013-10-22 00:15:45 +0200

    Hopefully Fix for wrong format of clock and countdown on projector

--- openslides/projector/static/javascript/clock.js
+++ openslides/projector/static/javascript/clock.js
@@ -2,6 +2,10 @@ function update_clock() {
     var currentTime = new Date();
     var currentHours = currentTime.getHours();
     var currentMinutes = currentTime.getMinutes();
+    if(currentMinutes < 10)
+    {
+        currentMinutes = "0" + currentMinutes
+    }
     $('#currentTime').html(currentHours + ':' + currentMinutes);
     setTimeout('update_clock()', 200);
 }
--- openslides/projector/static/javascript/countdown.js
+++ openslides/projector/static/javascript/countdown.js
@@ -5,6 +5,8 @@ function update_countdown() {
     var duration = projector.projector_countdown_duration;
     var pause = projector.projector_countdown_pause;
 
+
+
     switch (projector.projector_countdown_state) {
         case 'active':
             seconds = start + duration - time;
@@ -16,9 +18,24 @@ function update_countdown() {
             seconds = duration;
             break;
     }
+    if (seconds > 60 )
+    {
+        minutes = Math.floor(seconds / 60); 
+        secs = Math.floor(seconds - 60);
+        if(secs < 10)
+        {
+            secs = "0" + secs;
+        }
+        seconds = minutes + ":" + secs;
+   }
+   else
+   {
+       seconds = Math.max(0,Math.floor(seconds));
+   }
     if (seconds !== undefined) {
-        seconds = Math.max(0, Math.floor(seconds));
+        //seconds = Math.max(0, Math.floor(seconds));
         $('#overlay_countdown_inner').html(seconds);
+        
     }
     setTimeout('update_countdown()', 200);
 }
