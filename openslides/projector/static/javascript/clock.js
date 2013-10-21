53a0cf99c9d9de304fe37b729c77ba4ddcd28d37
Dominik Breu <dominikbreu@yahoo.de>
2013-10-22 01:29:48 +0200

    Make clock look like 09:03 instead of 9:03 as in piror commit

--- openslides/projector/static/javascript/clock.js
+++ openslides/projector/static/javascript/clock.js
@@ -2,11 +2,19 @@ function update_clock() {
     var currentTime = new Date();
     var currentHours = currentTime.getHours();
     var currentMinutes = currentTime.getMinutes();
-    if(currentMinutes < 10)
-    {
-        currentMinutes = "0" + currentMinutes
-    }
+    currentHours = normalise(currentHours);
+    currentMinutes = normalise(currentMinutes);
+
     $('#currentTime').html(currentHours + ':' + currentMinutes);
     setTimeout('update_clock()', 200);
 }
+
 update_clock();
+function normalise(i)
+{
+    if(i < 10)
+    {
+        i = "0" + i;
+    }
+    return i;
+}
