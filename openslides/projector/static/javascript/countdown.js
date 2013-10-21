12c7534e593002c173916070d75958e2b02351ea
Dominik Breu <dominikbreu@yahoo.de>
2013-10-22 01:24:55 +0200

    Fix for missing logic for countdown more then 120 and 3600 seconds

--- openslides/projector/static/javascript/countdown.js
+++ openslides/projector/static/javascript/countdown.js
@@ -1,6 +1,8 @@
 function update_countdown() {
     var time = new Date().getTime() / 1000;
     var seconds;
+    var minutes;
+    var secs;
     var start = projector.projector_countdown_start;
     var duration = projector.projector_countdown_duration;
     var pause = projector.projector_countdown_pause;
@@ -18,25 +20,50 @@ function update_countdown() {
             seconds = duration;
             break;
     }
-    if (seconds > 60 )
+    if (seconds > 60 && seconds < 3600 )
     {
         minutes = Math.floor(seconds / 60); 
-        secs = Math.floor(seconds - 60);
-        if(secs < 10)
-        {
-            secs = "0" + secs;
-        }
-        seconds = minutes + ":" + secs;
+        secs = Math.floor(seconds -  (minutes* 60));
+       
+       minutes = normalise(minutes);
+       secs = normalise(secs);
+        
+       seconds =  minutes+ ":" + secs;
    }
    else
    {
        seconds = Math.max(0,Math.floor(seconds));
    }
+   if(seconds >= 3600)
+   {
+       hours = Math.floor(seconds / 3600);
+       
+       minutes = Math.floor((seconds - (hours * 3600)) / 60);
+
+       secs = Math.floor(seconds -  (hours * 3600) - (minutes * 60) );
+
+       hours = normalise(hours);
+       minutes = normalise(minutes);
+       secs = normalise(secs);
+       
+       seconds = hours + ":"+ minutes + ":" + secs;
+       
+   }
+   
     if (seconds !== undefined) {
-        //seconds = Math.max(0, Math.floor(seconds));
         $('#overlay_countdown_inner').html(seconds);
         
     }
     setTimeout('update_countdown()', 200);
 }
 update_countdown();
+
+function normalise(i)
+{
+    if(i < 10)
+    {
+        i = "0" + i;
+    }
+    return i;
+}
+
