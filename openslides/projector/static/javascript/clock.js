6e46b2221b5b081e69f30cdec04a99b374ea4f74
Dominik Breu <dominikbreu@yahoo.de>
2013-10-22 18:55:30 +0200

    Codestyle fix

--- openslides/projector/static/javascript/clock.js
+++ openslides/projector/static/javascript/clock.js
@@ -1,20 +1,16 @@
-function update_clock()
-{
+function update_clock() {
     var currentTime = new Date();
     var currentHours = currentTime.getHours();
     var currentMinutes = currentTime.getMinutes();
     currentHours = normalise(currentHours);
     currentMinutes = normalise(currentMinutes);
-
     $('#currentTime').html(currentHours + ':' + currentMinutes);
     setTimeout('update_clock()', 200);
 }
 
 update_clock();
-function normalise(i)
-{
-    if(i < 10)
-    {
+function normalise(i) {
+    if(i < 10) {
         i = "0" + i;
     }
     return i;
--- openslides/projector/static/javascript/countdown.js
+++ openslides/projector/static/javascript/countdown.js
@@ -1,5 +1,4 @@
-function update_countdown()
-{
+function update_countdown() {
     var time = new Date().getTime() / 1000;
     var seconds;
     var minutes_digit;
@@ -8,8 +7,7 @@ function update_countdown()
     var start = projector.projector_countdown_start;
     var duration = projector.projector_countdown_duration;
     var pause = projector.projector_countdown_pause;
-    switch(projector.projector_countdown_state)
-    {
+    switch(projector.projector_countdown_state) {
         case 'active':
             seconds = start + duration - time;
             break;
@@ -20,40 +18,30 @@ function update_countdown()
             seconds = duration;
             break;
     }
-    if(seconds > 60)
-    {
+    if(seconds > 60) {
         hours_digit    = Math.floor(seconds / 3600);
         minutes_digit  = Math.floor((seconds - (hours_digit * 3600)) / 60);
         seconds_digit  = Math.floor(seconds - (hours_digit * 3600) - (minutes_digit * 60));
         minutes_digit  = normalise(minutes_digit);
         seconds_digit  = normalise(seconds_digit);
-        if(hours_digit > 0)
-        {
+        if(hours_digit > 0) {
             hours_digit  = normalise(hours_digit);
             seconds      = hours_digit + ":" + minutes_digit + ":" + seconds_digit;
-        }
-        else
-        {
+        } else {
             seconds = minutes_digit + ":" + seconds_digit;
         }
-    }
-    else
-    {
+    } else {
         seconds = Math.max(0, Math.floor(seconds));
     }
-    if(seconds !== undefined)
-    {
+    if(seconds != = undefined) {
         $('#overlay_countdown_inner').html(seconds);
-        
     }
     setTimeout('update_countdown()', 200);
 }
 update_countdown();
 
-function normalise(i)
-{
-    if(i < 10)
-    {
+function normalise(i) {
+    if(i < 10) {
         i = "0" + i;
     }
     return i;
