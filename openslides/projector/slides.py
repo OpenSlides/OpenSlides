1fb0a21d0b4156cde18b6e85d8a4aa4d7dd2f86b
Emanuel Schuetze <emanuel@intevation.de>
2013-11-06 00:11:13 +0100

    Some more template fixes:
    - Fixed #992 (Use correct countdown format)
    - Fixed #909 (Backend und projector template style)
      * Search bar with same border-radius like login button.
      * Projector: Fixed overlay message style. Allow HTML code (use template filter 'safe').
      * Dashboard: Tooltips, Buttons and Widget fixes.
    - Fixed template does not exists error if custom slide is activated.
    - Countdown: Add css to show negative countdown in red.
    - Add missing translation strings to English po file (for transifex).

--- openslides/projector/slides.py
+++ openslides/projector/slides.py
@@ -4,4 +4,4 @@ from openslides.projector.api import register_slide_model
 
 from .models import ProjectorSlide
 
-register_slide_model(ProjectorSlide, 'projector/slide_projectorslide.html')
+register_slide_model(ProjectorSlide, 'projector/projectorslide_slide.html')
--- openslides/projector/static/javascript/countdown.js
+++ openslides/projector/static/javascript/countdown.js
@@ -1,51 +1,44 @@
 function update_countdown() {
     var time = projector.get_server_time().getTime() / 1000;
-    var seconds;
-    var minutes_digit;
-    var seconds_digit;
-    var hours_digit;
+    var totalseconds;
+    var min;
+    var sec;
+    var negative;
     var start = projector.projector_countdown_start;
     var duration = projector.projector_countdown_duration;
     var pause = projector.projector_countdown_pause;
 
     switch (projector.projector_countdown_state) {
         case 'active':
-            seconds = start + duration - time;
+            totalseconds = start + duration - time;
             break;
         case 'paused':
-            seconds = start + duration - pause;
+            totalseconds = start + duration - pause;
             break;
         case 'inactive':
-            seconds = duration;
+            totalseconds = duration;
             break;
     }
-    if (seconds > 60) {
-        hours_digit    = Math.floor(seconds / 3600);
-        minutes_digit  = Math.floor((seconds - (hours_digit * 3600)) / 60);
-        seconds_digit  = Math.floor(seconds - (hours_digit * 3600) - (minutes_digit * 60));
-        minutes_digit  = normalise(minutes_digit);
-        seconds_digit  = normalise(seconds_digit);
-        if (hours_digit > 0) {
-            hours_digit  = normalise(hours_digit);
-            seconds      = hours_digit + ":" + minutes_digit + ":" + seconds_digit;
-        } else {
-            seconds = minutes_digit + ":" + seconds_digit;
-        }
-    } else {
-        seconds = Math.max(0, Math.floor(seconds));
+    totalseconds = Math.floor(totalseconds);
+    if (totalseconds < 0 ) {
+        totalseconds = -totalseconds;
+        negative = true;
     }
+    min = Math.floor(totalseconds / 60);
+    sec = Math.floor(totalseconds - (min * 60));
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
+    if (negative) {
+        min = "-" + min;
+        $('#overlay_countdown_inner').addClass('negative');
+    }
+    else {
+        $('#overlay_countdown_inner').removeClass('negative');
+    }
+    if(totalseconds !== undefined) {
+        $('#overlay_countdown_inner').html(min + ":" + sec);
 
-    if(seconds !== undefined) {
-        $('#overlay_countdown_inner').html(seconds);
     }
 }
-
 setInterval('update_countdown()', 200);
-
-function normalise(i) {
-    if(i < 10) {
-        i = "0" + i;
-    }
-    return i;
-}
-
