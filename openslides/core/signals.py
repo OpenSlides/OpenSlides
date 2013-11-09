34701c67a6ed61674785cff008ed1790ff67a1ba
Emanuel Schuetze <emanuel@intevation.de>
2013-11-09 23:04:50 +0100

    Template improvements.
    - motion detail template: Fixes motion manage state template block.
    - mediafile list template: Fixed missing activeline class.
    - item detail template: Fixed is_active_slide conditions.
    - assignment view template: Use bottongroup instead of single radio buttons.
    - Projector template: Updated header and countdown style. Removed footer.
    - Added config options to show logo and/or title/description.
    - Projector template: New config options to set background color and font color for projector header.
    - Make WLAN form labels not translateable.

--- openslides/core/signals.py
+++ openslides/core/signals.py
@@ -59,6 +59,48 @@ def setup_general_config_page(sender, **kwargs):
             label=ugettext_lazy('Event organizer'),
             required=False))
 
+    projector_enable_logo = ConfigVariable(
+        name='projector_enable_logo',
+        default_value=True,
+        form_field=forms.BooleanField(
+            label=ugettext_lazy('Show logo on projector'),
+            help_text=ugettext_lazy('You can find and replace the logo under "openslides/static/img/projector/static/img/logo-projector.png".'),
+            required=False))
+
+    projector_enable_title = ConfigVariable(
+        name='projector_enable_title',
+        default_value=True,
+        form_field=forms.BooleanField(
+            label=ugettext_lazy('Show titel and description of event on projector'),
+            required=False))
+
+    projector_backgroundcolor1 = ConfigVariable(
+        name='projector_backgroundcolor1',
+        default_value='#444444',
+        form_field=forms.CharField(
+            widget=forms.TextInput(),
+            label=ugettext_lazy('Background color of projector header'),
+            help_text=ugettext_lazy('Use web color names like "red" or hex numbers like "#ff0000".'),
+            required=True))
+
+    projector_backgroundcolor2 = ConfigVariable(
+        name='projector_backgroundcolor2',
+        default_value='#222222',
+        form_field=forms.CharField(
+            widget=forms.TextInput(),
+            label=ugettext_lazy('Second (optional) background color for linear color gradient'),
+            help_text=ugettext_lazy('Use web color names like "red" or hex numbers like "#ff0000".'),
+            required=False))
+
+    projector_fontcolor = ConfigVariable(
+        name='projector_fontcolor',
+        default_value='#F5F5F5',
+        form_field=forms.CharField(
+            widget=forms.TextInput(),
+            label=ugettext_lazy('Font color of projector header'),
+            help_text=ugettext_lazy('Use web color names like "red" or hex numbers like "#ff0000".'),
+            required=True))
+
     welcome_title = ConfigVariable(
         name='welcome_title',
         default_value=_('Welcome to OpenSlides'),
@@ -119,14 +161,18 @@ def setup_general_config_page(sender, **kwargs):
             label=ugettext_lazy('WLAN encryption'),
             help_text=ugettext_lazy('Used for WLAN QRCode in PDF of access data.'),
             choices=(
-                ('', ugettext_lazy('---------')),
-                ('WEP', ugettext_lazy('WEP')),
-                ('WPA', ugettext_lazy('WPA/WPA2')),
+                ('', '---------'),
+                ('WEP', 'WEP'),
+                ('WPA', 'WPA/WPA2'),
                 ('nopass', ugettext_lazy('No encryption')))))
 
     group_event = ConfigGroup(
         title=ugettext_lazy('Event'),
         variables=(event_name, event_description, event_date, event_location, event_organizer))
+
+    group_projector = ConfigGroup(
+        title=ugettext_lazy('Projector'),
+        variables=(projector_enable_logo, projector_enable_title, projector_backgroundcolor1, projector_backgroundcolor2, projector_fontcolor))
 
     group_welcome_widget = ConfigGroup(
         title=ugettext_lazy('Welcome Widget'),
@@ -141,4 +187,4 @@ def setup_general_config_page(sender, **kwargs):
         url='general',
         required_permission='config.can_manage',
         weight=10,
-        groups=(group_event, group_welcome_widget, group_system))
+        groups=(group_event, group_projector, group_welcome_widget, group_system))
