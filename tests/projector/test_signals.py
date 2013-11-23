a77650b4a8acc95bc7f39c24ea0c167cee8d7ca9
Oskar Hahn <mail@oshahn.de>
2013-11-23 12:17:46 +0100

    Fixed blinking countdown

---
+++ tests/projector/test_signals.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from mock import MagicMock, patch
+
+from openslides.projector.signals import countdown
+from openslides.utils.test import TestCase
+
+
+class CountdownTest(TestCase):
+    def test_order_of_get_projector_js(self):
+        """
+        Tests that the order of the js values is in the right order. Especially
+        the value 'call' has to come at the end.
+        """
+        overlay = countdown('fake sender')
+        test_value = overlay.get_javascript()
+
+        self.assertIsInstance(test_value, dict)
+        self.assertEqual(
+            test_value.keys(),
+            ['load_file', 'projector_countdown_start',
+             'projector_countdown_duration', 'projector_countdown_pause',
+             'projector_countdown_state', 'call'])
