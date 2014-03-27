9abb7a3cafaac213adae95897e8d92add4846496
Norman Jäckel <openslides@normanjaeckel.de>
2014-03-27 22:57:13 +0100

    Updated CHANGELOG, README.rst, requirements.txt, requirements_production.txt and setup.py.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
 import re
+import sys
 
 from setuptools import setup, find_packages
 
@@ -16,7 +16,7 @@ with open('README.rst') as readme:
 dependency_line = re.compile('[^#-]')
 with open('requirements_production.txt') as requirements_production:
     install_requires = [line for line in requirements_production.readlines()
-                             if dependency_line.match(line)]
+                        if dependency_line.match(line)]
 
 
 setup(
@@ -31,11 +31,10 @@ setup(
         # http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
-        'Intended Audience :: Other Audience',
         'Framework :: Django',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
     ],
     license='MIT',
     packages=find_packages(exclude=['tests', 'tests.*']),
