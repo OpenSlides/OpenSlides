5b60201c1b46179a7edb902920d683d50be778c3
Oskar Hahn <mail@oshahn.de>
2014-08-26 01:40:01 +0200

    art

--- make/commands.py
+++ make/commands.py
@@ -106,3 +106,53 @@ def download_js_requirements(args=None):
         path = os.path.join(directory, requirement[0])
         with open(path, 'wb') as f:
             f.write(response.read())
+
+
+@command('art', help='For NJ')
+def art(args=None):
+    print("""
+                                    WNKOON
+                                  WNKOOKN
+                                WNKOOKN
+                              WN0OOKN
+                            WX0kOKN
+                          WX0kOKW
+                        WXOxOKW
+  WNXXKX              WX0kx0W
+  KOXKxxX           WX00N NKXXW
+  00WXkxK          X0KNNXNWWWXKKXXN
+  0kX0xd0          0ONWNKKKXNWNKKOO0KW
+WX0OxdxkOKXXXXXXXNNkkXNNXKKKKKNNK00OkO0XW
+X00o'.,d0kxOO000000dxXXNXKK0000KXX0OkkxxkOX
+NK0d;,;x0xxkkkkkkOOodKKXXK0000OOO0K0kkkkxddx0W
+ N0kkxkkOXWWWWWWWWWOxKKKKK000OOOOkOOOkkxxxxcoN
+  OxKOoo0          OkKK0000OOOkkkkxxxxxddoo:oX
+  OkN0ooK          XOOOOdoddddddddoooolcoxddON
+  OxN0ooK               XO0XKKKK0000OkoxN
+  OxX0ooK                NXNNNNXXXXXXXXW
+  OdX0loK
+  OdKOloK
+  OdKOllK
+  OdKOclK
+  Oo0OclK
+  Oo0kclK
+  OlOkclK
+  OlOkclK
+  OlOkcl0
+KX0k0Okk0KKKKKKKKKKKKKKK000000000000OOOOOOOOOOOOkkkkkkkkkkkxxxx
+OK0000000OOOOOOOOOOkkkkkkkkkkkxxxxxxxxxxxxdddddddddoooooooooool
+xOkddddoooooooooolllllllllllcccccccccccc:::::::::;;;;;;;;;;;;:o
+OKkocccccccc::::::::;;;;;;;;;,,,,,,,,,,'''''''''............';O
+kOxolllcccccccccccc:::::::::::;;;;;;;;;;;,,,,,,,,'''''''....';k
+kOkkkkkkkkxxxxxxxxxxxxxdddddddddddooooooooooollllllllcccccccccl
+KOkOxlllcccccccc::;::;;:;;;;;,;;,,,,,,,,,,,,,,,','''''''''',lk0
+ WKOkoc::::;;;;,''''''''''''..............................,dX
+  W0xxdllllllccccccccccc:::::::::::;;;;;;;;;;;;,,,,,,,''';dX
+  N0OOkOkxddddddddddoooooooooolllllllllllccccccccc::::ccodxX
+   WNXOOxc;;,,,,,,,,''''''''''........................,oKXN
+      WK0xc;,,,,,,,'''''''''.........................;xN
+        XOxol:;,,'''''''''..........................:OW
+         NXK0koc:,,''''.........................,cdOXW
+             NXOl:;::;;,,''.............'''''''oXW
+               K;..'oOkxxdlc:::;;;::clodxOOl...d
+               Nd;;l0    NX000OOOO00KNW    0c,;O               """)
