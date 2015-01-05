f7cb518430d3f2e3627251e36001d9250300f1df
Norman Jäckel <openslides@normanjaeckel.de>
2015-01-05 15:01:42 +0100

    Used Bower and gulp to manage third party JavaScript and Cascading Style Sheets libraries.
    Cleaned up settings. Updated development section in README.rst.
    
    Removed jQuery, jquery.cookie and bootstrap from repository.

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+/*!
+ * Gulp tasks for development and production.
+ *
+ * Run
+ *
+ *      $ ./node_modules/.bin/gulp
+ *
+ * for development and
+ *
+ *      $ ./node_modules/.bin/gulp --production
+ *
+ * for production mode.
+ */
+
+var argv = require('yargs').argv,
+    gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    gulpif = require('gulp-if'),
+    mainBowerFiles = require('main-bower-files'),
+    minifyCSS = require('gulp-minify-css'),
+    path = require('path'),
+    uglify = require('gulp-uglify');
+
+// Directory where the results go
+var output_directory = path.join('openslides', 'static');
+
+// Catches all JavaScript files from all bower components and concats them to
+// one file js/openslides-libs.js. In production mode the file is uglified.
+gulp.task('js', function() {
+    return gulp.src(mainBowerFiles({
+            filter: /\.js$/
+        }))
+        .pipe(concat('openslides-libs.js'))
+        .pipe(gulpif(argv.production, uglify()))
+        .pipe(gulp.dest(path.join(output_directory, 'js')));
+});
+
+// Catches all CSS files from all bower components and concats them to one file
+// css/openslides-libs.css. In production mode the file is uglified.
+gulp.task('css', function() {
+    return gulp.src(mainBowerFiles({
+            filter: /\.css$/
+        }))
+        .pipe(concat('openslides-libs.css'))
+        .pipe(gulpif(argv.production, minifyCSS()))
+        .pipe(gulp.dest(path.join(output_directory, 'css')));
+});
+
+// Catches all PNG image files from all bower components.
+gulp.task('img', function() {
+    return gulp.src(mainBowerFiles({
+            filter: /\.png$/
+        }))
+        .pipe(gulp.dest(path.join(output_directory, 'img')));
+});
+
+// Gulp default task. Runs all other tasks before.
+gulp.task('default', ['js', 'css', 'img'], function() {});
--- tests/settings.py
+++ tests/settings.py
@@ -1,53 +1,67 @@
-#
-# Settings file for OpenSlides' tests
-#
+"""
+Settings file for OpenSlides' tests
+"""
 
 import os
 from openslides.global_settings import *  # noqa
 
 # Path to the directory for user specific data files
+
 OPENSLIDES_USER_DATA_PATH = os.path.realpath(os.path.dirname(__file__))
 
-# Use 'DEBUG = True' to get more details for server errors. Default for releases: False
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
 
-# Make this unique, and don't share it with anybody.
+# SECURITY WARNING: Keep the secret key used in production secret!
+
 SECRET_KEY = 'secret'
 
-# Database settings. Change this to use MySQL or PostgreSQL
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': ''}}
 
-# Add OpenSlides plugins to this list
-INSTALLED_PLUGINS = (
+# Use 'DEBUG = True' to get more details for server errors.
+# SECURITY WARNING: Don't run with debug turned on in production!
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+
+# OpenSlides plugins
+# Add plugins to this list.
+
+INSTALLED_PLUGINS += (
     'tests.utils',
 )
 
 INSTALLED_APPS += INSTALLED_PLUGINS
 
+
+# Database
+# Change this to use MySQL or PostgreSQL.
+# See https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ''
+    }
+}
+
+
 # Some other settings
+
 TIME_ZONE = 'Europe/Berlin'
 
 MEDIA_ROOT = os.path.join(OPENSLIDES_USER_DATA_PATH, '')
 
-HAYSTACK_CONNECTIONS['default']['STORAGE'] = 'ram'
-
 TEMPLATE_DIRS = (
     os.path.join(OPENSLIDES_USER_DATA_PATH, 'templates'),
-    os.path.join(SITE_ROOT, 'templates'))
+)
 
-STATICFILES_DIRS = (
-    os.path.join(OPENSLIDES_USER_DATA_PATH, 'static'),
-    os.path.join(SITE_ROOT, 'static'))
+STATICFILES_DIRS.insert(0, os.path.join(OPENSLIDES_USER_DATA_PATH, 'static'))
 
-# Use a faster passwort hasher
+HAYSTACK_CONNECTIONS['default']['STORAGE'] = 'ram'
+
+
+# Special test settings
+# Use a faster password hasher.
+
 PASSWORD_HASHERS = (
     'django.contrib.auth.hashers.MD5PasswordHasher',
 )
