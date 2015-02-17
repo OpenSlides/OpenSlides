cfdabfbb406e12ddb6a57d62ac63bb608bd59079
Oskar Hahn <mail@oshahn.de>
2015-02-17 20:07:44 +0100

    Added utils.views.CSRFMixin to append the csrf token to a view
    Fixes: #1476

--- tests/unit/utils/test_views.py
+++ tests/unit/utils/test_views.py
@@ -331,3 +331,16 @@ class TestAPIView(TestCase):
         self.assertFalse(
             hasattr(views.APIView, 'method_call'),
             "The APIView should not have the method 'method_call'")
+
+
+class TestCSRFMixin(TestCase):
+    @patch('builtins.super')
+    def test_as_view(self, mock_super):
+        """
+        Tests, that ensure_csrf_cookie is called.
+        """
+        mock_super().as_view.return_value = 'super_view'
+        with patch('openslides.utils.views.ensure_csrf_cookie') as ensure_csrf_cookie:
+            views.CSRFMixin.as_view()
+
+        ensure_csrf_cookie.assert_called_once_with('super_view')
