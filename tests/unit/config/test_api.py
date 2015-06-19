2b9c0b4620f55b814715a33d4f1260bec7688c5c
Oskar Hahn <mail@oshahn.de>
2015-06-19 09:08:36 +0200

    Fixed default_value in option request

---
+++ tests/unit/config/test_api.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+from unittest.mock import patch
+
+from openslides.config.api import ConfigVariable
+
+
+class TestConfigVariable(TestCase):
+    @patch('openslides.config.api.config', {'test_variable': None})
+    def test_default_value_in_data(self):
+        """
+        Tests, that the default_value attribute is in the 'data' property of
+        a ConfigVariable instance.
+        """
+        config_variable = ConfigVariable('test_variable', 'test_default_value')
+
+        self.assertIn(
+            'default_value',
+            config_variable.data,
+            "Config_varialbe.data should have a key 'default_value'")
+        self.assertEqual(
+            config_variable.data['default_value'],
+            'test_default_value',
+            "The value of config_variable.data['default_value'] should be the same "
+            "as set as second argument of ConfigVariable()")
