89a6d5b451b92c7ed63eafde882c31b7b3e3fab1
Oskar Hahn <mail@oshahn.de>
2015-07-07 10:09:35 +0200

    Appended an _id suffix to all related field names in the rest api
    Fixes #1597

--- openslides/utils/rest_api.py
+++ openslides/utils/rest_api.py
@@ -1,4 +1,5 @@
 import re
+from collections import OrderedDict
 from urllib.parse import urlparse
 
 from rest_framework.decorators import detail_route, list_route  # noqa
@@ -6,14 +7,16 @@ from rest_framework.metadata import SimpleMetadata  # noqa
 from rest_framework.mixins import DestroyModelMixin, UpdateModelMixin  # noqa
 from rest_framework.response import Response  # noqa
 from rest_framework.routers import DefaultRouter
+from rest_framework.serializers import ModelSerializer as _ModelSerializer
 from rest_framework.serializers import (  # noqa
+    MANY_RELATION_KWARGS,
     CharField,
     DictField,
     Field,
     IntegerField,
     ListField,
     ListSerializer,
-    ModelSerializer,
+    ManyRelatedField,
     PrimaryKeyRelatedField,
     RelatedField,
     SerializerMethodField,
@@ -28,6 +31,83 @@ from rest_framework.viewsets import ViewSet as _ViewSet  # noqa
 from .exceptions import OpenSlidesError
 
 router = DefaultRouter()
+
+
+class IdManyRelatedField(ManyRelatedField):
+    """
+    ManyRelatedField that appends an suffix to the sub-fields.
+
+    Only works together with the IdPrimaryKeyRelatedField and our
+    ModelSerializer.
+    """
+    field_name_suffix = '_id'
+
+    def bind(self, field_name, parent):
+        """
+        Called when the field is bound to the serializer.
+
+        See IdPrimaryKeyRelatedField for more informations.
+        """
+        self.source = field_name[:-len(self.field_name_suffix)]
+        super().bind(field_name, parent)
+
+
+class IdPrimaryKeyRelatedField(PrimaryKeyRelatedField):
+    """
+    Field, that renames the field name to FIELD_NAME_id.
+
+    Only works together the our ModelSerializer.
+    """
+    field_name_suffix = '_id'
+
+    def bind(self, field_name, parent):
+        """
+        Called when the field is bound to the serializer.
+
+        Changes the source so that the original field name is used (removes
+        the _id suffix).
+        """
+        if field_name:
+            # field_name is an empty string when the field is created with the
+            # attribute many=True. In this case the suffix is added with the
+            # IdManyRelatedField class.
+            self.source = field_name[:-len(self.field_name_suffix)]
+        super().bind(field_name, parent)
+
+    @classmethod
+    def many_init(cls, *args, **kwargs):
+        """
+        Method from rest_framework.relations.RelatedField That uses our
+        IdManyRelatedField class instead of
+        rest_framework.relations.ManyRelatedField class.
+        """
+        list_kwargs = {'child_relation': cls(*args, **kwargs)}
+        for key in kwargs.keys():
+            if key in MANY_RELATION_KWARGS:
+                list_kwargs[key] = kwargs[key]
+        return IdManyRelatedField(**list_kwargs)
+
+
+class ModelSerializer(_ModelSerializer):
+    """
+    ModelSerializer that changes the field names of related fields to
+    FIELD_NAME_id.
+    """
+    serializer_related_field = IdPrimaryKeyRelatedField
+
+    def get_fields(self):
+        """
+        Returns all fields of the serializer.
+        """
+        fields = OrderedDict()
+
+        for field_name, field in super().get_fields().items():
+            try:
+                field_name += field.field_name_suffix
+            except AttributeError:
+                pass
+            fields[field_name] = field
+        return fields
 
 
 class PermissionMixin:
