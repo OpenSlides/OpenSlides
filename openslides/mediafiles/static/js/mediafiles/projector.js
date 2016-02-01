af023f30bcc0fc1c209fe7ec0f5bd9933fac31e1
Emanuel Schuetze <emanuel@intevation.de>
2016-02-01 12:51:37 +0100

    Mediafile: Improvements for pdf presentaiton and list view.

--- openslides/mediafiles/static/js/mediafiles/projector.js
+++ openslides/mediafiles/static/js/mediafiles/projector.js
@@ -23,35 +23,6 @@ angular.module('OpenSlidesApp.mediafiles.projector', ['OpenSlidesApp.mediafiles'
             $scope.pdfName = mediafile.title;
             $scope.pdfUrl = mediafile.mediafileUrl;
         })
-        // get page from projector
-        $scope.page = $scope.element.page;
-        $scope.scroll = 0;
-
-        function updateScale() {
-            if($scope.element.pageFit) {
-                $scope.scale = 'page-fit';
-            } else {
-                $scope.scale = $scope.element.scale;
-            }
-        }
-
-        $scope.$watch(function() {
-            return $scope.element.scale;
-        }, updateScale);
-
-        updateScale();
-
-        $scope.getNavStyle = function(scroll) {
-            if (scroll > 100) {
-                return 'pdf-controls fixed';
-            } else {
-                return 'pdf-controls';
-            }
-        };
-
-        $scope.onLoad = function() {
-            $scope.loading = '';
-        };
     }
 ]);
 
