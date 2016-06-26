d215860412610c484be705c3491b373bc78a4896
Tobias Hößl <tobias@hoessl.eu>
2016-06-26 20:28:05 +0200

    Line breaking in text nodes

--- openslides/motions/static/js/motions/linenumbering.js
+++ openslides/motions/static/js/motions/linenumbering.js
@@ -16,6 +16,9 @@ angular.module('OpenSlidesApp.motions', [])
 
 
 .service('lineNumberingService', function lineNumberingService() {
+    var ELEMENT_NODE = 1,
+        TEXT_NODE = 3;
+
 
     this.helloWorld = function() {
         return 'Hello World';
@@ -27,8 +30,98 @@ angular.module('OpenSlidesApp.motions', [])
         this.lineLength = length;
     };
 
+    this._textNodeToLines = function(node, length, offset) {
+        var out = [],
+            currLineStart = 0,
+            currLineOffset = offset,
+            i = 0,
+            firstTextNode = true,
+            lastBreakableIndex = null;
+
+        var addLine = function(text) {
+                var newNode = document.createTextNode(text);
+                if (firstTextNode) {
+                    firstTextNode = false;
+                } else {
+                    var br = document.createElement('br');
+                    out.push(br);
+                }
+                out.push(newNode)
+            };
+
+        // This happens if a previous inline element exactly stretches to the end of the line
+        if (currLineOffset >= length) {
+            var br = document.createElement('br');
+            out.push(br);
+            currLineOffset = 0;
+        }
+
+        while (i < node.nodeValue.length) {
+            var lineBreakAt = null;
+            if (currLineOffset == length) {
+                if (lastBreakableIndex) {
+                    lineBreakAt = lastBreakableIndex;
+                } else {
+                    lineBreakAt = i - 1;
+                }
+            }
+            if (lineBreakAt) {
+                var currLine = node.nodeValue.substring(currLineStart, lineBreakAt + 1);
+                addLine(currLine);
+
+                currLineStart = lineBreakAt + 1;
+                currLineOffset = i - lineBreakAt - 1;
+                lastBreakableIndex = null;
+            }
+
+            currLineOffset++;
+            i++;
+
+            if (node.nodeValue[i] == ' ' || node.nodeValue[i] == '-') {
+                lastBreakableIndex = i;
+            }
+        }
+        addLine(node.nodeValue.substring(currLineStart));
+
+        return { "nodes": out, "newOffset": currLineOffset };
+    };
+
+    this._insertLineNumbersToNode = function(node, length) {
+        var out = [],
+            i, j, lines;
+        if (node.nodeType == TEXT_NODE) {
+            var ret = this._textNodeToLines(node, length, 0);
+            for (i in ret.nodes) {
+                out.push(ret.nodes[i]);
+            }
+        } else if (node.nodeType == ELEMENT_NODE) {
+            for (i in node.childNodes) {
+                lines = this._insertLineNumbersToNode(node.childNodes[i]);
+                for (j in lines) {
+                    out.push(lines[j]);
+                }
+            }
+        } else {
+            throw "Unknown nodeType: " + node.nodeType;
+        }
+        return out;
+    };
+
+    this._nodesToHtml = function (nodes) {
+        var root = document.createElement('div');
+        for (var i in nodes) {
+            root.appendChild(nodes[i]);
+        }
+        return root.innerHTML;
+    };
+
     this.insertLineNumbers = function(html) {
-        // @TODO
+        var root = document.createElement('div');
+        root.innerHTML = html;
+
+        var out = this._insertLineNumbersToNode(root, this.lineLength);
+
+        return this._nodesToHtml(out);
     }
 });
 
--- tests/karma/motions/linenumbering.service.test.js
+++ tests/karma/motions/linenumbering.service.test.js
@@ -13,7 +13,54 @@ describe('linenumbering', function () {
         var out = lineNumberingService.helloWorld();
         expect(out).toBe('Hello World');
     });
+    
+    it('breaks very short lines', function() {
+      var textNode = document.createTextNode("0123");
+      var out = lineNumberingService._textNodeToLines(textNode, 5, 0);
+      var outHtml = lineNumberingService._nodesToHtml(out.nodes);
+      expect(outHtml).toBe('0123');
+      expect(out.newOffset).toBe(4);
+    });
 
+    it('breaks simple lines', function() {
+      var textNode = document.createTextNode("012345678901234567");
+      var out = lineNumberingService._textNodeToLines(textNode, 5, 0);
+      var outHtml = lineNumberingService._nodesToHtml(out.nodes);
+      expect(outHtml).toBe('01234<br>56789<br>01234<br>567');
+      expect(out.newOffset).toBe(3);
+    });
+
+    it('breaks simple lines with offset', function() {
+      var textNode = document.createTextNode("012345678901234567");
+      var out = lineNumberingService._textNodeToLines(textNode, 5, 2);
+      var outHtml = lineNumberingService._nodesToHtml(out.nodes);
+      expect(outHtml).toBe('012<br>34567<br>89012<br>34567');
+      expect(out.newOffset).toBe(5);
+    });
+
+    it('breaks simple lines with offset equaling to length', function() {
+      var textNode = document.createTextNode("012345678901234567");
+      var out = lineNumberingService._textNodeToLines(textNode, 5, 5);
+      var outHtml = lineNumberingService._nodesToHtml(out.nodes);
+      expect(outHtml).toBe('<br>01234<br>56789<br>01234<br>567');
+      expect(out.newOffset).toBe(3);
+    });
+
+    it('breaks simple lines with spaces (1)', function() {
+      var textNode = document.createTextNode("0123 45 67 89012 34 567");
+      var out = lineNumberingService._textNodeToLines(textNode, 5, 0);
+      var outHtml = lineNumberingService._nodesToHtml(out.nodes);
+      expect(outHtml).toBe('0123 <br>45 67 <br>89012 <br>34 <br>567');
+      expect(out.newOffset).toBe(3);
+    });
+
+    it('breaks simple lines with spaces (2)', function() {
+      var textNode = document.createTextNode("0123 45 67 89012tes 344 ");
+      var out = lineNumberingService._textNodeToLines(textNode, 5, 0);
+      var outHtml = lineNumberingService._nodesToHtml(out.nodes);
+      expect(outHtml).toBe('0123 <br>45 67 <br>89012<br>tes <br>344 ');
+      expect(out.newOffset).toBe(4);
+    });
   });
 
 });
