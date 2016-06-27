14c4bbb7a3e14b870a2973d16710f8b851e74d92
Tobias Hößl <tobias@hoessl.eu>
2016-06-27 23:13:39 +0200

    Initial support for inline elements

--- openslides/motions/static/js/motions/linenumbering.js
+++ openslides/motions/static/js/motions/linenumbering.js
@@ -25,15 +25,32 @@ angular.module('OpenSlidesApp.motions', [])
     };
 
     this.lineLength = 80;
+    this._currentInlineOffset = null;
 
     this.setLineLength = function(length) {
         this.lineLength = length;
     };
 
-    this._textNodeToLines = function(node, length, offset) {
+    this._isInlineElement = function(node) {
+        var inlineElements = [
+            'SPAN', 'A', 'EM', 'S', 'B', 'I', 'STRONG', 'U', 'BIG', 'SMALL', 'SUB', 'SUP', 'TT'
+        ];
+        return (inlineElements.indexOf(node.nodeName) > -1);
+    };
+
+    /**
+     * Splits a TEXT_NODE into an array of TEXT_NODEs and BR-Elements separating them into lines.
+     * Each line has a maximum length of "length", with one exception: spaces are accepted to exceed the length.
+     * Otherwise the string is split by the last space or dash in the line.
+     *
+     * @param node
+     * @param length
+     * @returns Array
+     * @private
+     */
+    this._textNodeToLines = function(node, length) {
         var out = [],
             currLineStart = 0,
-            currLineOffset = offset,
             i = 0,
             firstTextNode = true,
             lastBreakableIndex = null;
@@ -44,21 +61,23 @@ angular.module('OpenSlidesApp.motions', [])
                     firstTextNode = false;
                 } else {
                     var br = document.createElement('br');
+                    br.setAttribute("class", "os-line-break");
                     out.push(br);
                 }
                 out.push(newNode)
             };
 
         // This happens if a previous inline element exactly stretches to the end of the line
-        if (currLineOffset >= length) {
+        if (this._currentInlineOffset >= length) {
             var br = document.createElement('br');
+            br.setAttribute("class", "os-line-break");
             out.push(br);
-            currLineOffset = 0;
+            this._currentInlineOffset = 0;
         }
 
         while (i < node.nodeValue.length) {
             var lineBreakAt = null;
-            if (currLineOffset >= length) {
+            if (this._currentInlineOffset >= length) {
                 if (lastBreakableIndex) {
                     lineBreakAt = lastBreakableIndex;
                 } else {
@@ -70,7 +89,7 @@ angular.module('OpenSlidesApp.motions', [])
                 addLine(currLine);
 
                 currLineStart = lineBreakAt + 1;
-                currLineOffset = i - lineBreakAt - 1;
+                this._currentInlineOffset = i - lineBreakAt - 1;
                 lastBreakableIndex = null;
             }
 
@@ -78,34 +97,55 @@ angular.module('OpenSlidesApp.motions', [])
                 lastBreakableIndex = i;
             }
 
-            currLineOffset++;
+            this._currentInlineOffset++;
             i++;
 
         }
         addLine(node.nodeValue.substring(currLineStart));
 
-        return { "nodes": out, "newOffset": currLineOffset };
+        return out;
+    };
+
+
+    this._insertLineNumbersToInlineNode = function(node, length) {
+        var oldChildren = [];
+        for (var i = 0; i < node.childNodes.length; i++) {
+            oldChildren.push(node.childNodes[i]);
+        }
+
+        while (node.firstChild) {
+            node.removeChild(node.firstChild);
+        }
+
+        for (var i = 0; i < oldChildren.length; i++) {
+            if (oldChildren[i].nodeType == TEXT_NODE) {
+                var ret = this._textNodeToLines(oldChildren[i], length);
+                for (var j in ret) {
+                    node.appendChild(ret[j]);
+                }
+            } else if (oldChildren[i].nodeType == ELEMENT_NODE) {
+                node.appendChild(
+                    this._insertLineNumbersToNode(oldChildren[i], length)
+                );
+            } else {
+                throw "Unknown nodeType: " + i + ": " + oldChildren[i];
+            }
+        }
+
+        return node;
     };
 
     this._insertLineNumbersToNode = function(node, length) {
-        var out = [],
-            i, j, lines;
-        if (node.nodeType == TEXT_NODE) {
-            var ret = this._textNodeToLines(node, length, 0);
-            for (i in ret.nodes) {
-                out.push(ret.nodes[i]);
-            }
-        } else if (node.nodeType == ELEMENT_NODE) {
-            for (i in node.childNodes) {
-                lines = this._insertLineNumbersToNode(node.childNodes[i]);
-                for (j in lines) {
-                    out.push(lines[j]);
-                }
-            }
-        } else {
-            throw "Unknown nodeType: " + node.nodeType;
+        if (node.nodeType !== ELEMENT_NODE) {
+            throw "This method may only be called for ELEMENT-nodes: " + node.nodeValue;
         }
-        return out;
+        if (this._isInlineElement(node)) {
+            return this._insertLineNumbersToInlineNode(node, length);
+        } else {
+            // @TODO
+            this._currentInlineOffset = 0;
+            return this._insertLineNumbersToInlineNode(node, length);
+        }
     };
 
     this._nodesToHtml = function (nodes) {
@@ -120,9 +160,10 @@ angular.module('OpenSlidesApp.motions', [])
         var root = document.createElement('div');
         root.innerHTML = html;
 
-        var out = this._insertLineNumbersToNode(root, this.lineLength);
+        this._currentInlineOffset = 0;
+        var newRoot = this._insertLineNumbersToNode(root, this.lineLength);
 
-        return this._nodesToHtml(out);
+        return newRoot.innerHTML;
     }
 });
 
--- tests/karma/motions/linenumbering.service.test.js
+++ tests/karma/motions/linenumbering.service.test.js
@@ -16,68 +16,80 @@ describe('linenumbering', function () {
     
     it('breaks very short lines', function() {
       var textNode = document.createTextNode("0123");
-      var out = lineNumberingService._textNodeToLines(textNode, 5, 0);
-      var outHtml = lineNumberingService._nodesToHtml(out.nodes);
+      lineNumberingService._currentInlineOffset = 0;
+      var out = lineNumberingService._textNodeToLines(textNode, 5);
+      var outHtml = lineNumberingService._nodesToHtml(out);
       expect(outHtml).toBe('0123');
-      expect(out.newOffset).toBe(4);
+      expect(lineNumberingService._currentInlineOffset).toBe(4);
     });
 
     it('breaks simple lines', function() {
       var textNode = document.createTextNode("012345678901234567");
-      var out = lineNumberingService._textNodeToLines(textNode, 5, 0);
-      var outHtml = lineNumberingService._nodesToHtml(out.nodes);
-      expect(outHtml).toBe('01234<br>56789<br>01234<br>567');
-      expect(out.newOffset).toBe(3);
+      lineNumberingService._currentInlineOffset = 0;
+      var out = lineNumberingService._textNodeToLines(textNode, 5);
+      var outHtml = lineNumberingService._nodesToHtml(out);
+      expect(outHtml).toBe('01234<br class="os-line-break">56789<br class="os-line-break">01234<br class="os-line-break">567');
+      expect(lineNumberingService._currentInlineOffset).toBe(3);
     });
 
     it('breaks simple lines with offset', function() {
       var textNode = document.createTextNode("012345678901234567");
-      var out = lineNumberingService._textNodeToLines(textNode, 5, 2);
-      var outHtml = lineNumberingService._nodesToHtml(out.nodes);
-      expect(outHtml).toBe('012<br>34567<br>89012<br>34567');
-      expect(out.newOffset).toBe(5);
+      lineNumberingService._currentInlineOffset = 2;
+      var out = lineNumberingService._textNodeToLines(textNode, 5);
+      var outHtml = lineNumberingService._nodesToHtml(out);
+      expect(outHtml).toBe('012<br class="os-line-break">34567<br class="os-line-break">89012<br class="os-line-break">34567');
+      expect(lineNumberingService._currentInlineOffset).toBe(5);
     });
 
     it('breaks simple lines with offset equaling to length', function() {
       var textNode = document.createTextNode("012345678901234567");
-      var out = lineNumberingService._textNodeToLines(textNode, 5, 5);
-      var outHtml = lineNumberingService._nodesToHtml(out.nodes);
-      expect(outHtml).toBe('<br>01234<br>56789<br>01234<br>567');
-      expect(out.newOffset).toBe(3);
+      lineNumberingService._currentInlineOffset = 5;
+      var out = lineNumberingService._textNodeToLines(textNode, 5);
+      var outHtml = lineNumberingService._nodesToHtml(out);
+      expect(outHtml).toBe('<br class="os-line-break">01234<br class="os-line-break">56789<br class="os-line-break">01234<br class="os-line-break">567');
+      expect(lineNumberingService._currentInlineOffset).toBe(3);
     });
 
     it('breaks simple lines with spaces (1)', function() {
       var textNode = document.createTextNode("0123 45 67 89012 34 567");
-      var out = lineNumberingService._textNodeToLines(textNode, 5, 0);
-      var outHtml = lineNumberingService._nodesToHtml(out.nodes);
-      expect(outHtml).toBe('0123 <br>45 67 <br>89012 <br>34 <br>567');
-      expect(out.newOffset).toBe(3);
+      lineNumberingService._currentInlineOffset = 0;
+      var out = lineNumberingService._textNodeToLines(textNode, 5);
+      var outHtml = lineNumberingService._nodesToHtml(out);
+      expect(outHtml).toBe('0123 <br class="os-line-break">45 67 <br class="os-line-break">89012 <br class="os-line-break">34 <br class="os-line-break">567');
+      expect(lineNumberingService._currentInlineOffset).toBe(3);
     });
 
     it('breaks simple lines with spaces (2)', function() {
       var textNode = document.createTextNode("0123 45 67 89012tes 344 ");
-      var out = lineNumberingService._textNodeToLines(textNode, 5, 0);
-      var outHtml = lineNumberingService._nodesToHtml(out.nodes);
-      expect(outHtml).toBe('0123 <br>45 67 <br>89012<br>tes <br>344 ');
-      expect(out.newOffset).toBe(4);
+      lineNumberingService._currentInlineOffset = 0;
+      var out = lineNumberingService._textNodeToLines(textNode, 5);
+      var outHtml = lineNumberingService._nodesToHtml(out);
+      expect(outHtml).toBe('0123 <br class="os-line-break">45 67 <br class="os-line-break">89012<br class="os-line-break">tes <br class="os-line-break">344 ');
+      expect(lineNumberingService._currentInlineOffset).toBe(4);
     });
 
     it('breaks simple lines with spaces (3)', function() {
       var textNode = document.createTextNode("I'm a Demo-Text");
-      var out = lineNumberingService._textNodeToLines(textNode, 5, 0);
-      var outHtml = lineNumberingService._nodesToHtml(out.nodes);
-      expect(outHtml).toBe('I\'m a <br>Demo-<br>Text');
-      expect(out.newOffset).toBe(4);
+      lineNumberingService._currentInlineOffset = 0;
+      var out = lineNumberingService._textNodeToLines(textNode, 5);
+      var outHtml = lineNumberingService._nodesToHtml(out);
+      expect(outHtml).toBe('I\'m a <br class="os-line-break">Demo-<br class="os-line-break">Text');
+      expect(lineNumberingService._currentInlineOffset).toBe(4);
     });
 
     it('breaks simple lines with spaces (4)', function() {
       var textNode = document.createTextNode("I'm a LongDemo-Text");
-      var out = lineNumberingService._textNodeToLines(textNode, 5, 0);
-      var outHtml = lineNumberingService._nodesToHtml(out.nodes);
-      expect(outHtml).toBe('I\'m a <br>LongD<br>emo-<br>Text');
-      expect(out.newOffset).toBe(4);
+      lineNumberingService._currentInlineOffset = 0;
+      var out = lineNumberingService._textNodeToLines(textNode, 5);
+      var outHtml = lineNumberingService._nodesToHtml(out);
+      expect(outHtml).toBe('I\'m a <br class="os-line-break">LongD<br class="os-line-break">emo-<br class="os-line-break">Text');
+      expect(lineNumberingService._currentInlineOffset).toBe(4);
     });
 
+    it('should leave a simple SPAN untouched', function() {
+      var outHtml = lineNumberingService.insertLineNumbers("<span>Test</span>");
+      expect(outHtml).toBe('<span>Test</span>');
+    });
   });
 
 });
