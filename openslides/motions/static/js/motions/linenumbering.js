652701e4f3005393ebb14778c739bd82ac3baca7
Tobias Hößl <tobias@hoessl.eu>
2018-03-15 19:34:42 +0100

    Add firstLine to cache definition - fixes #3656

--- openslides/motions/static/js/motions/linenumbering.js
+++ openslides/motions/static/js/motions/linenumbering.js
@@ -538,7 +538,8 @@ angular.module('OpenSlidesApp.motions.lineNumbering', [])
                 newRoot = this.insertLineNumbersNode(html, lineLength, highlight, firstLine);
                 newHtml = newRoot.innerHTML;
             } else {
-                var cacheKey = this.djb2hash(lineLength.toString() + html);
+                var firstLineStr = (firstLine === undefined || firstLine === null ? '' : firstLine.toString());
+                var cacheKey = this.djb2hash(firstLineStr + "-" + lineLength.toString() + html);
                 newHtml = lineNumberCache.get(cacheKey);
 
                 if (angular.isUndefined(newHtml)) {
