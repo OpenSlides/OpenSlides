595bcab84577b2ed68c97ee7bfedff7a2c485d5e
FinnStutzenstein <finn.stutzenstein@hotmail.de>
2018-04-11 13:09:11 +0200

    Added shortened motion title to motion block slide.

--- openslides/motions/static/js/motions/motion-block-projector.js
+++ openslides/motions/static/js/motions/motion-block-projector.js
@@ -26,6 +26,33 @@ angular.module('OpenSlidesApp.motions.motionBlockProjector', [])
         // class.
         var id = $scope.element.id;
         MotionBlock.bindOne(id, $scope, 'motionBlock');
+
+        // Returns a shortened motion title. If the title is longer then maxLength, it is
+        // split at the last whitespace that is in maxLength. Three dots are added then.
+        $scope.getShortTitle = function (motion) {
+            var maxLength = 40;
+            var title = motion.getTitle();
+
+            if (title.length <= maxLength) {
+                return title;
+            }
+
+            // Find last whitespace that is before maxLength. Split the title
+            // there and append dots.
+            var whitespaceIndex = -1;
+            for (var i = 0; i < maxLength+1; i++) {
+                if (title[i] === ' ') {
+                    whitespaceIndex = i;
+                }
+            }
+
+            if (whitespaceIndex === -1) {
+                // just one long word.. split it :/
+                return title.substr(0, maxLength) + '...';
+            } else {
+                return title.substr(0, whitespaceIndex) + '...';
+            }
+        };
     }
 ]);
 
