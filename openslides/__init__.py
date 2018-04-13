03b17837ed2c88692f1b99ec5b9b477f86fdddb6
Emanuel Schütze <emanuel@intevation.de>
2018-04-13 16:08:01 +0200

    Update version to 2.2b4-dev

--- openslides/__init__.py
+++ openslides/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'OpenSlides Team <support@openslides.org>'
 __description__ = 'Presentation and assembly system'
-__version__ = '2.2b3'
+__version__ = '2.2b4-dev'
 __license__ = 'MIT'
 __url__ = 'https://openslides.org'
 
