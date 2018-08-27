3b5d2f9ea467390d33077f32fad6b03b28cc344a
Emanuel Schütze <emanuel@intevation.de>
2018-08-27 09:07:59 +0200

    Added motion category list view.

--- client/src/app/site/motions/motions.module.ts
+++ client/src/app/site/motions/motions.module.ts
@@ -5,9 +5,10 @@ import { MotionsRoutingModule } from './motions-routing.module';
 import { SharedModule } from '../../shared/shared.module';
 import { MotionListComponent } from './motion-list/motion-list.component';
 import { MotionDetailComponent } from './motion-detail/motion-detail.component';
+import { CategoryListComponent } from './category-list/category-list.component';
 
 @NgModule({
     imports: [CommonModule, MotionsRoutingModule, SharedModule],
-    declarations: [MotionListComponent, MotionDetailComponent]
+    declarations: [MotionListComponent, MotionDetailComponent, CategoryListComponent]
 })
 export class MotionsModule {}
