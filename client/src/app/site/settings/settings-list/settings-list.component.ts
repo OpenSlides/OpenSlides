72ddacd8a27c5523c530cd588540d1a7e1b5fd0e
Sean Engelhardt <sean.f.t.engelhardt@gmail.com>
2018-08-28 13:54:25 +0200

    Add reusable head bar component
    - implement the head bar in all apps
    - work on the documentation

--- client/src/app/site/settings/settings-list/settings-list.component.ts
+++ client/src/app/site/settings/settings-list/settings-list.component.ts
@@ -3,16 +3,29 @@ import { Title } from '@angular/platform-browser';
 import { TranslateService } from '@ngx-translate/core';
 import { BaseComponent } from '../../../base.component';
 
+/**
+ * List view for the global settings
+ *
+ * TODO: Not yet implemented
+ */
 @Component({
     selector: 'app-settings-list',
     templateUrl: './settings-list.component.html',
     styleUrls: ['./settings-list.component.css']
 })
 export class SettingsListComponent extends BaseComponent implements OnInit {
+    /**
+     * The usual component constructor
+     * @param titleService
+     * @param translate
+     */
     constructor(titleService: Title, protected translate: TranslateService) {
         super(titleService, translate);
     }
 
+    /**
+     * Init function. Sets the title
+     */
     ngOnInit() {
         super.setTitle('Settings');
     }
