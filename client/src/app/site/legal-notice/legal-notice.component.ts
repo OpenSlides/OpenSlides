698f3d83fec8fdb6793d17750597e3478775f740
FinnStutzenstein <finn.stutzenstein@hotmail.de>
2018-09-04 12:33:21 +0200

    ConfigService

--- client/src/app/site/legal-notice/legal-notice.component.ts
+++ client/src/app/site/legal-notice/legal-notice.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from '../../core/services/config.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'os-legal-notice',
@@ -6,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./legal-notice.component.scss']
 })
 export class LegalNoticeComponent implements OnInit {
-    public constructor() {}
+    public legalNotice: string;
 
-    public ngOnInit() {}
+    public constructor(private configService: ConfigService, private translate: TranslateService) {}
+
+    public ngOnInit() {
+        this.configService.get('general_event_legal_notice').subscribe(value => {
+            if (value) {
+                this.legalNotice = this.translate.instant(value);
+            }
+        });
+    }
 }
