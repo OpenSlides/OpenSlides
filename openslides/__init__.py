dca94333e80263dba32f9d1b12119ac70d85583e
Emanuel Schütze <emanuel@intevation.de>
2018-09-20 09:26:27 +0200

    Update version to 2.3.1-dev

--- openslides/__init__.py
+++ openslides/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'OpenSlides Team <support@openslides.org>'
 __description__ = 'Presentation and assembly system'
-__version__ = '2.3'
+__version__ = '2.3.1-dev'
 __license__ = 'MIT'
 __url__ = 'https://openslides.org'
 
