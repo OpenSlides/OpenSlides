c704b90ecb773c4ddb8c1e0b66a5bb500ad384b1
Emanuel Schütze <emanuel@intevation.de>
2019-01-20 10:56:50 +0100

    Fix history migration.

--- openslides/core/migrations/0013_auto_20190119_1641.py
+++ openslides/core/migrations/0013_auto_20190119_1641.py
@@ -5,6 +5,16 @@ import jsonfield.fields
 from django.db import migrations, models
 
 
+def clear_history(apps, schema_editor):
+    """
+    Deletes the full history.
+    """
+    # We get the model from the versioned app registry;
+    # if we directly import it, it will be the wrong version.
+    History = apps.get_model("core", "History")
+    History.objects.all().delete()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -17,6 +27,7 @@ class Migration(migrations.Migration):
             name='restricted',
             field=models.BooleanField(default=False),
         ),
+        migrations.RunPython(clear_history),
         migrations.AlterField(
             model_name='history',
             name='information',
