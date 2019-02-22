efe8f3e86263c4d4d2aaf52fbf1a799612b53965
GabrielMeyer <meyergabriel@live.de>
2019-02-22 10:41:27 +0100

    Adds sanitizer to render text correctly
    Sanitized the text, which is displayed, to show formatting like
    centering, bold etc.
    
    Sanitized the text for projectors

--- client/src/app/slides/topics/topic/topic-slide.component.ts
+++ client/src/app/slides/topics/topic/topic-slide.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { BaseSlideComponent } from 'app/slides/base-slide-component';
 import { TopicSlideData } from './topic-slide-data';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
     selector: 'os-topic-slide',
@@ -8,7 +9,19 @@ import { TopicSlideData } from './topic-slide-data';
     styleUrls: ['./topic-slide.component.scss']
 })
 export class TopicSlideComponent extends BaseSlideComponent<TopicSlideData> {
-    public constructor() {
+    public constructor(private sanitizer: DomSanitizer) {
         super();
+    }
+
+    /**
+     * Function to sanitize text.
+     * Necessary to render the text correctly.
+     *
+     * @param text which should be displayed.
+     *
+     * @returns safeHtml which can be displayed.
+     */
+    public sanitizedText(text: string): SafeHtml {
+        return this.sanitizer.bypassSecurityTrustHtml(text);
     }
 }
