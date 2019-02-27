e870ed495a7692bb932d8feede8253fbd6fa3e01
Maximilian Krambach <maximilian.krambach@intevation.de>
2019-02-27 17:50:22 +0100

    fix identifier setting on mass setstate

--- openslides/motions/views.py
+++ openslides/motions/views.py
@@ -666,9 +666,18 @@ class MotionViewSet(ModelViewSet):
                     {"detail": f"You can not set the state to {state_id}."}
                 )
             motion.set_state(state_id)
+            state = State.objects.get(pk=state_id)
 
             # Save motion.
-            motion.save(update_fields=["state", "last_modified"], skip_autoupdate=True)
+            if not state.dont_set_identifier:
+                motion.save(
+                    update_fields=["state", "last_modified", "identifier"],
+                    skip_autoupdate=True,
+                )
+            else:
+                motion.save(
+                    update_fields=["state", "last_modified"], skip_autoupdate=True
+                )
 
             # Write the log message.
             motion.write_log(
