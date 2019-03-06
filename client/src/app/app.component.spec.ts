e1fcda78901852085322d06c5419adcca10d8b24
FinnStutzenstein <finn.stutzenstein@hotmail.de>
2019-03-06 07:38:43 +0100

    Fixed SW registration issues due to pending mikrotasks

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, fakeAsync, tick } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { E2EImportsModule } from './../e2e-imports.module';
 import { ServertimeService } from './core/core-services/servertime.service';
+import { TranslateService } from '@ngx-translate/core';
 
 describe('AppComponent', () => {
-    let servertimeService;
+    let servertimeService, translate;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -12,12 +13,21 @@ describe('AppComponent', () => {
         }).compileComponents();
 
         servertimeService = TestBed.get(ServertimeService);
+        translate = TestBed.get(TranslateService);
         spyOn(servertimeService, 'startScheduler').and.stub();
+        spyOn(translate, 'addLangs').and.stub();
+        spyOn(translate, 'setDefaultLang').and.stub();
+        spyOn(translate, 'getBrowserLang').and.stub();
+        spyOn(translate, 'getLangs').and.returnValue([]);
+        spyOn(translate, 'use').and.stub();
     }));
-    it('should create the app', async(() => {
+    it('should create the app', fakeAsync(() => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app).toBeTruthy();
-        expect(servertimeService.startScheduler).toHaveBeenCalled();
+        tick();
+        fixture.whenStable().then(() => {
+            expect(servertimeService.startScheduler).toHaveBeenCalled();
+        });
     }));
 });
