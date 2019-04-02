3be90d33b18a030883467e1c3cf8874625b54d8c
francisduvivier <francisduvivier@users.noreply.github.com>
2019-04-03 00:10:54 +0200

    More flexible parsing of font sizes and margins
    I noticed that I got NaN for font-size in case my text style was for example "font-size: 12px".
    That is because +'12px' results in NaN
    When using parseFloat instead, 12px would just become 12 instead of NaN.
    parseFloat is available on all browsers according to https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseFloat , so its safe to use...

--- client/src/app/core/ui-services/html-to-pdf.service.ts
+++ client/src/app/core/ui-services/html-to-pdf.service.ts
@@ -603,11 +603,11 @@ export class HtmlToPdfService {
                 if (styleDefinition.length === 2) {
                     switch (key) {
                         case 'padding-left': {
-                            styleObject.margin = [+value, 0, 0, 0];
+                            styleObject.margin = [parseFloat(value), 0, 0, 0];
                             break;
                         }
                         case 'font-size': {
-                            styleObject.fontSize = +value;
+                            styleObject.fontSize = parseFloat(value);
                             break;
                         }
                         case 'text-align': {
