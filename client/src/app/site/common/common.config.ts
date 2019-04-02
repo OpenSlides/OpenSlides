2c73b9b781906386580546135bf8aaba10fa5b5c
FinnStutzenstein <finn.stutzenstein@hotmail.de>
2019-04-02 12:28:19 +0200

    Remove the ChatMessage from the client

--- client/src/app/site/common/common.config.ts
+++ client/src/app/site/common/common.config.ts
@@ -1,18 +1,7 @@
 import { AppConfig } from '../../core/app-config';
-import { ChatMessage } from '../../shared/models/core/chat-message';
-import { ChatMessageRepositoryService } from 'app/core/repositories/common/chatmessage-repository.service';
-import { ViewChatMessage } from './models/view-chatmessage';
 
 export const CommonAppConfig: AppConfig = {
     name: 'common',
-    models: [
-        {
-            collectionString: 'core/chat-message',
-            model: ChatMessage,
-            viewModel: ViewChatMessage,
-            repository: ChatMessageRepositoryService
-        }
-    ],
     mainMenuEntries: [
         {
             route: '/',
