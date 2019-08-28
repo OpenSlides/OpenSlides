79e879cb6ef9b9fc3d305063b0b82d73f8d8fdeb
GabrielMeyer <meyergabriel@live.de>
2019-08-28 14:59:30 +0200

    Adds config to see a max number of next speakers
    - Also updates the 'list of speakers' on the `overlay-slide`.

--- client/src/app/slides/agenda/current-list-of-speakers-overlay/current-list-of-speakers-overlay-slide.component.ts
+++ client/src/app/slides/agenda/current-list-of-speakers-overlay/current-list-of-speakers-overlay-slide.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { BaseSlideComponent } from 'app/slides/base-slide-component';
 import { CommonListOfSpeakersSlideData } from '../common/common-list-of-speakers-slide-data';
+
+/**
+ * Interface, that describes how the speaker-objects look like.
+ */
+interface SpeakerObject {
+    user: string;
+    marked: boolean;
+    end_time: number | null;
+    weight: number | null;
+}
 
 @Component({
     selector: 'os-current-list-of-speakers-overlay-slide',
@@ -9,6 +19,31 @@ import { CommonListOfSpeakersSlideData } from '../common/common-list-of-speakers
     styleUrls: ['./current-list-of-speakers-overlay-slide.component.scss']
 })
 export class CurrentListOfSpeakersOverlaySlideComponent extends BaseSlideComponent<CommonListOfSpeakersSlideData> {
+    /**
+     * Gets the data. Sets necessary information for the list of speakers in the overlay.
+     *
+     * @param data The passed data to this overlay.
+     */
+    @Input()
+    public set data(data: any) {
+        if (data.data.current) {
+            this.currentSpeaker = data.data.current;
+        }
+        if (data.data.waiting) {
+            this.nextSpeakers = data.data.waiting;
+        }
+    }
+
+    /**
+     * The current speaker.
+     */
+    public currentSpeaker: SpeakerObject;
+
+    /**
+     * List with the next speakers for this list.
+     */
+    public nextSpeakers: SpeakerObject[] = [];
+
     public constructor() {
         super();
     }
