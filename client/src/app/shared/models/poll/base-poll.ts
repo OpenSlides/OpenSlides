eec086e9edeac5df66aad9b0d12894cd48c0f91f
jsangmeister <joshua.sangmeister@gmail.com>
2019-11-05 12:56:24 +0100

    wip

--- client/src/app/shared/models/poll/base-poll.ts
+++ client/src/app/shared/models/poll/base-poll.ts
@@ -8,11 +8,54 @@ export enum PollState {
     Published
 }
 
+export const PollStateVerbose = {
+    1: 'Created',
+    2: 'Started',
+    3: 'Finished',
+    4: 'Published'
+};
+
 export enum PollType {
     Analog = 'analog',
     Named = 'named',
     Pseudoanonymous = 'pseudoanonymous'
 }
+
+export const PollTypeVerbose = {
+    analog: 'Analog',
+    named: 'Named',
+    pseudoanonymous: 'Pseudoanonymous'
+};
+
+export enum PercentBase {
+    YN = 'YN',
+    YNA = 'YNA',
+    Valid = 'valid',
+    Cast = 'cast',
+    Disabled = 'disabled'
+}
+
+export const PercentBaseVerbose = {
+    YN: 'Yes/No',
+    YNA: 'Yes/No/Abstain',
+    valid: 'Valid votes',
+    cast: 'Casted votes',
+    disabled: 'Disabled'
+};
+
+export enum MajorityMethod {
+    Simple = 'simple',
+    TwoThirds = 'two_thirds',
+    ThreeQuarters = 'three_quarters',
+    Disabled = 'disabled'
+}
+
+export const MajorityMethodVerbose = {
+    simple: 'Simple',
+    two_thirds: 'Two Thirds',
+    three_quarters: 'Three Quarters',
+    disabled: 'Disabled'
+};
 
 export interface BasePollWithoutNestedModels {
     state: PollState;
@@ -23,6 +66,8 @@ export interface BasePollWithoutNestedModels {
     votescast: number;
     groups_id: number[];
     voted_id: number[];
+    majority_method: MajorityMethod;
+    onehundred_percent_base: PercentBase;
 }
 
 export abstract class BasePoll<T, O extends BaseOption<any>> extends BaseDecimalModel<T> {
@@ -30,6 +75,22 @@ export abstract class BasePoll<T, O extends BaseOption<any>> extends BaseDecimal
 
     protected getDecimalFields(): (keyof BasePoll<T, O>)[] {
         return ['votesvalid', 'votesinvalid', 'votescast'];
+    }
+
+    public get stateVerbose(): string {
+        return PollStateVerbose[this.state];
+    }
+
+    public get typeVerbose(): string {
+        return PollTypeVerbose[this.type];
+    }
+
+    public get majorityMethodVerbose(): string {
+        return MajorityMethodVerbose[this.majority_method];
+    }
+
+    public get percentBaseVerbose(): string {
+        return PercentBaseVerbose[this.onehundred_percent_base];
     }
 }
 export interface BasePoll<T, O extends BaseOption<any>> extends BasePollWithoutNestedModels {}
