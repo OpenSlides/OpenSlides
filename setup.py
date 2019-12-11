48c25d1e5bd53d40fd3488ea3de280874ebd46b6
Sean Engelhardt <sean.f.t.engelhardt@gmail.com>
2019-12-11 16:12:58 +0100

    Use file-based database during testing

--- setup.py
+++ setup.py
@@ -8,38 +8,39 @@ from openslides import __version__ as openslides_version
 from openslides import __license__ as openslides_license
 from openslides import __url__ as openslides_url
 
-with open('README.rst') as readme:
+with open("README.rst") as readme:
     long_description = readme.read()
 
-with open('requirements/production.txt') as requirements_production:
+with open("requirements/production.txt") as requirements_production:
     install_requires = requirements_production.readlines()
 
-with open('requirements/big_mode.txt') as requirements_big_mode:
+with open("requirements/big_mode.txt") as requirements_big_mode:
     extras_requires = requirements_big_mode.readlines()
 
 setup(
-    name='openslides',
+    name="openslides",
     author=openslides_author,
-    author_email='support@openslides.com',
+    author_email="support@openslides.com",
     description=openslides_description,
     license=openslides_license,
     long_description=long_description,
     url=openslides_url,
     version=openslides_version,
-
     classifiers=[
         # http://pypi.python.org/pypi?%3Aaction=list_classifiers
         # 'Development Status :: 3 - Alpha',
-        'Development Status :: 4 - Beta',
+        "Development Status :: 4 - Beta",
         # 'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7', ],
-    packages=find_packages(exclude=['tests', 'tests.*']),
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(exclude=["tests", "tests.*"]),
     include_package_data=True,
     install_requires=install_requires,
-    extras_require={'big_mode': extras_requires},
-    entry_points={'console_scripts': ['openslides = openslides.__main__:main']})
+    extras_require={"big_mode": extras_requires},
+    entry_points={"console_scripts": ["openslides = openslides.__main__:main"]},
+)
