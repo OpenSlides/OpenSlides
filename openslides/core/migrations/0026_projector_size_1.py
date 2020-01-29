72494d876e1f26bb7091f60751d6f2fddbb619b1
FinnStutzenstein <finn.stutzenstein@hotmail.de>
2020-01-29 15:41:32 +0100

    Fixed order of migrations

--- openslides/core/migrations/0026_projector_size_1.py
+++ openslides/core/migrations/0026_projector_size_1.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("core", "0026_remove_history_restricted"),
+        ("core", "0025_projector_color"),
     ]
 
     operations = [
