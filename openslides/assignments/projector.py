a3a9e7d447dd5e4c354395bae7341a98647048c4
homer54 <karsten.kropp@web.de>
2020-04-23 22:09:02 +0200

    Bugfix amount_global_abstain var
    Prevents exception if poll doesn't allow abstain votes.

--- openslides/assignments/projector.py
+++ openslides/assignments/projector.py
@@ -76,7 +76,7 @@ async def assignment_poll_slide(
             float(poll["amount_global_no"]) if poll["amount_global_no"] else None
         )
         poll_data["amount_global_abstain"] = (
-            float(poll["amount_global_abstain"]) if poll["amount_global_no"] else None
+            float(poll["amount_global_abstain"]) if poll["amount_global_abstain"] else None
         )
         poll_data["votesvalid"] = float(poll["votesvalid"])
         poll_data["votesinvalid"] = float(poll["votesinvalid"])
