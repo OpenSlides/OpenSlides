63c4bc3ff71fd29c0dfde8322a893b9ebe2feb9b
Gernot Schulz <gernot@intevation.de>
2020-08-14 14:39:01 +0200

    build.sh: Build config from template

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 ../server/build.sh -t openslides/openslides-server:latest
 ../client/build.sh -t openslides/openslides-client:latest
+
+[[ -f docker-compose.yml ]] || m4 < docker-compose.yml.m4 > docker-compose.yml
+
 docker-compose build
