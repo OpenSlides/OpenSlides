1917ce32f59a55f4b580579f79d1373b6255c563
Jochen Winzer <jw@winzer-sw.de>
2020-11-12 19:24:51 +0100

    Vote@Home Version 3.3.2

--- server/openslides/__init__.py
+++ server/openslides/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "OpenSlides Team <support@openslides.org>"
 __description__ = "Presentation and assembly system"
-__version__ = "3.3.1-votehome"
+__version__ = "3.3.2-votehome"
 __license__ = "MIT"
 __url__ = "https://vote-at-home.de"
 
