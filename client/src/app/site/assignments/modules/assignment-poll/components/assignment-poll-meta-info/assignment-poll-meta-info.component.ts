9c738b5d8e67039430fa686832706c051b171373
Sean <sean.f.t.engelhardt@gmail.com>
2020-12-01 13:46:12 +0100

    Add poll meta info component
    Adds a component to display the assignment polls meta info

---
+++ client/src/app/site/assignments/modules/assignment-poll/components/assignment-poll-meta-info/assignment-poll-meta-info.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input } from '@angular/core';
+
+import { ViewAssignmentOption } from 'app/site/assignments/models/view-assignment-option';
+import { ViewAssignmentPoll } from 'app/site/assignments/models/view-assignment-poll';
+import { UnknownUserLabel } from 'app/site/assignments/modules/assignment-poll/services/assignment-poll.service';
+import { PollPropertyVerbose } from 'app/site/polls/models/view-base-poll';
+
+@Component({
+    selector: 'os-assignment-poll-meta-info',
+    templateUrl: './assignment-poll-meta-info.component.html',
+    styleUrls: ['./assignment-poll-meta-info.component.scss']
+})
+export class AssignmentPollMetaInfoComponent {
+    public pollPropertyVerbose = PollPropertyVerbose;
+    private unknownUserLabel = UnknownUserLabel;
+
+    @Input()
+    public poll: ViewAssignmentPoll;
+
+    @Input()
+    public showCandidates = true;
+
+    public get hasGlobalOption(): boolean {
+        return this.poll.hasGlobalOption;
+    }
+
+    public constructor() {}
+
+    public userCanVoe(): boolean {
+        // this.poll.canBeVotedFor
+        return true;
+    }
+
+    public getOptionTitle(option: ViewAssignmentOption): string {
+        return option.user?.getShortName().trim() ?? this.unknownUserLabel;
+    }
+}
