a37e2196b3b321a42149e92241f673776b399bfa
Emanuel Schütze <emanuel@intevation.de>
2020-12-18 00:17:27 +0100

    Release 3.3

--- server/openslides/__init__.py
+++ server/openslides/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "OpenSlides Team <support@openslides.org>"
 __description__ = "Presentation and assembly system"
-__version__ = "3.3-master"
+__version__ = "3.3"
 __license__ = "MIT"
 __url__ = "https://openslides.com"
 
