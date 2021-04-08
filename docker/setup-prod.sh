79a14e15adf11987554336d80fc503293509e5ef
Finn Stutzenstein <finn.stutzenstein@hotmail.de>
2021-04-08 08:54:10 +0200

    OS4 productive setup changes
    Now uses secrets and add the possibility to enable electronic voting

--- docker/setup-prod.sh
+++ docker/setup-prod.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Create keys for auth, if they do not exist
-if [ ! -d keys ]; then
-    mkdir keys
-
-    ssh-keygen -f keys/rsa-token.key -t rsa -b 2048 -P ""
-    ssh-keygen -f keys/rsa-cookie.key -t rsa -b 2048 -P ""
+if [ ! -f secrets/auth_token_key ]; then
+    tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 64 > secrets/auth_token_key
+fi
+if [ ! -f secrets/auth_cookie_key ]; then
+    tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 64 > secrets/auth_cookie_key
 fi
 
 ( set -a; source .env; m4 docker-compose.yml.m4 ) > docker-compose.yml
+( set -a; source .env; m4 docker-stack.yml.m4 ) > docker-stack.yml
