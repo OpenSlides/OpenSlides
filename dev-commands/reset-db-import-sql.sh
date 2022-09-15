23df16abf7937423e0f025262ff9baf87906e8de
peb-adr <adrichter97@gmail.com>
2022-09-15 11:34:04 +0200

    add script to import sql into dev setup (#6465)
    * add script to import sql into dev setup
    
    * Use import-events from DS + improvements
    
    Co-authored-by: Adrian Richter <adrian@intevation.de>

---
+++ dev-commands/reset-db-import-sql.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+DC="$(dirname $0)/../dc-dev.sh"
+FILE="$1"
+
+[ -n "$FILE" -a -r "$FILE" ] || {
+  echo "ERROR: cannot read file $FILE"
+  exit 1
+}
+
+$DC rm -svf postgres
+$DC up -d postgres
+# call cli script in the DS and pass local file via STDIN
+$DC exec -T datastore-writer 'cli/import-events.sh -' < "$FILE"
+# recreate autoupdate and depending services for output consistency
+$DC up -d --force-recreate autoupdate
