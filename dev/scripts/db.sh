da9de09ce3839833ba357bc90db3691242af47db
jsangmeister <33004050+jsangmeister@users.noreply.github.com>
2023-05-24 11:25:24 +0200

    Fix db.sh and hardcode password (#6595)

--- dev/scripts/db.sh
+++ dev/scripts/db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd "$(dirname $0)"
-docker-compose -f $HOME/../docker/docker-compose.dev.yml exec datastore-writer psql -h postgres -U openslides
+docker-compose -f ../docker/docker-compose.dev.yml exec datastore-writer psql postgresql://openslides:openslides@postgres/openslides
