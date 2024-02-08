8e46e2b468d26da3183f20d34faaa2898fe0a19a
Adrian Richter <adrian@intevation.de>
2024-02-08 16:08:56 +0100

    add `make-update hotfix` workflow

--- dev/scripts/make-update.sh
+++ dev/scripts/make-update.sh
@@ -11,7 +11,7 @@ OPT_PULL=
 OPT_LOCAL=
 
 # do not page diff and log outputs
-GIT_PAGER=
+export GIT_PAGER=
 
 usage() {
 cat <<EOF
@@ -128,7 +128,7 @@ fetch_all_changes() {
 }
 
 
-make_staging_update() {
+add_changes() {
   local REPLY= diff_cmd=
 
   set_remote
@@ -177,17 +177,47 @@ make_staging_update() {
     echo "No changes added."
     abort 0
   }
-  echo 'Changes to be included for the new staging update:'
+  echo 'Changes to be included for the new update:'
   echo '--------------------------------------------------------------------------------'
   $diff_cmd
   echo '--------------------------------------------------------------------------------'
-  ask y "Commit now?" && {
+}
+
+make_staging_update() {
+  local diff_cmd=
+
+  add_changes
+
+  ask y "Commit on branch $BRANCH_NAME for a new staging update now?" && {
     increment_patch &&
       git add VERSION
     git commit --message "Staging update $(date +%Y%m%d)"
     git show --no-patch
     echo "Commit created. Push to $REMOTE_NAME remote and PR into main repo to bring it live."
   } || :
+}
+
+make_hotfix_update() {
+  local diff_cmd=
+
+  add_changes
+
+  ask y "Commit on branch $BRANCH_NAME for a new stable (hotfix) update now?" && {
+    increment_patch &&
+      git add VERSION
+    git commit --message "Update $(cat VERSION) ($(date +%Y%m%d))"
+    git show --no-patch
+    echo "Commit created."
+  } || :
+
+  [[ -z "$OPT_LOCAL" ]] ||
+    return 0
+
+  echo "Attempting to push new $BRANCH_NAME commit into $REMOTE_NAME."
+  echo "Ensure you have proper access rights or use --local to skip pushing."
+  ask y "Continue?" ||
+    abort 0
+  git push "$REMOTE_NAME" "$BRANCH_NAME"
 }
 
 merge_stable_branches() {
@@ -315,6 +345,11 @@ for arg; do
     stable)
       BRANCH_NAME=$STABLE_BRANCH_NAME
       make_stable_update
+      shift 1
+      ;;
+    hotfix)
+      BRANCH_NAME=$STABLE_BRANCH_NAME
+      make_hotfix_update
       shift 1
       ;;
   esac
