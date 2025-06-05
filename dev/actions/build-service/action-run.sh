fcd3bb4130a968c408a6a8304a3c8db1c293fa2b
Jan Malte Behrens <janmtbehrens@gmail.com>
2025-06-05 16:03:03 +0200

    Almost works?

--- dev/actions/build-service/action-run.sh
+++ dev/actions/build-service/action-run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-printenv
-
 chmod +x ${GITHUB_ACTION_PATH}/../../scripts/makefile/build-service.sh
 ${GITHUB_ACTION_PATH}/../../scripts/makefile/build-service.sh ${SERVICE} ${CONTEXT} ${MODULE} ${PORT}
