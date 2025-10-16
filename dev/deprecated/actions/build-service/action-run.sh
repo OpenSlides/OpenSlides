a744cddcadbd1ad7dd98a362b4c3eb51020d27e0
Jan Malte Behrens <janmtbehrens@gmail.com>
2025-10-16 09:55:39 +0200

    Delete deprecated files and folders

--- dev/deprecated/actions/build-service/action-run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-chmod +x "${GITHUB_ACTION_PATH}"/../../scripts/makefile/build-service.sh
-"${GITHUB_ACTION_PATH}"/../../scripts/makefile/build-service.sh "${SERVICE}" "${CONTEXT}" "${MODULE}" "${PORT}"
