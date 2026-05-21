2966d5e522178761202d74a135b4a337352730bb
Jan Malte Behrens <janmtbehrens@gmail.com>
2026-05-21 17:30:06 +0200

    feat: Framework and first working examples of configuring realm-export.json automatically before starting keycloak

--- openslides-keycloak-service/entrypoint.sh
+++ openslides-keycloak-service/entrypoint.sh
@@ -4,14 +4,27 @@ set -e
 
 echo "Enter entrypoint.sh"
 
-echo "$KC_BOOTSTRAP_ADMIN_USERNAME $KC_BOOTSTRAP_ADMIN_PASSWORD $KC_HOSTNAME"
+# Config realm-export.json
+
+edit_realm_export()
+{
+    # Changes KEY in realm-export.json to VALUE. If no VALUE is given, KEY is taken as default instead
+    local KEY="${1:-}"
+    local VALUE="${2:-KEY}"
+    sed -i "s|$KEY|$VALUE|g" /opt/keycloak/data/import/realm-export.json
+}
+
+## Domain
+edit_realm_export "localhost" "$KCS_INSTANCE_URL"
+edit_realm_export "noreply@openslides.com" "$KCS_HOST_EMAIL"
+
 
 # Start Keycloak
 /opt/keycloak/bin/kc.sh start-dev --import-realm &
 
 # Wait for Keycloak to be ready
 until /opt/keycloak/bin/kcadm.sh config credentials \
-  --server "$KC_HOSTNAME" \
+  --server "http://${KCS_INSTANCE_URL}:8080" \
   --realm master \
   --user "$KC_BOOTSTRAP_ADMIN_USERNAME" \
   --password "$KC_BOOTSTRAP_ADMIN_PASSWORD" 2>/dev/null; do
@@ -25,7 +38,6 @@ echo "Keycloak is ready"
 
 # Set admin email
 ADMIN_USER=$(/opt/keycloak/bin/kcadm.sh get users -r master --query username="$KC_BOOTSTRAP_ADMIN_USERNAME" --fields id --format csv --noquotes)
-echo "$ADMIN_USER"
-/opt/keycloak/bin/kcadm.sh update users/"$ADMIN_USER" -r master -s email=noreply@openslides.com
+/opt/keycloak/bin/kcadm.sh update users/"$ADMIN_USER" -r master -s email="$KCS_HOST_EMAIL"
 
 exec "$@"
